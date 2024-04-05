9521e08ed4ae3b1194b8dcb058424e31e1dacfb4
ndrw-ynz <andrewyanza4@gmail.com>
2024-04-05 23:45:00 +0800

    feat: add initial script for player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        
+    }
+}
