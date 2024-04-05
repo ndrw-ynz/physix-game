8c30679dfe469d5035368de085173a59efc25e15
ndrw-ynz <andrewyanza4@gmail.com>
2024-04-05 23:42:59 +0800

    feat: add input reader for delegating input events

---
+++ Assets/Input/InputReader.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// The InputReader class is where all input events are delegated.
+/// </summary>
+[CreateAssetMenu(menuName = "InputReader")]
+public class InputReader : ScriptableObject, GameInput.IGameplayActions, GameInput.IUIActions
+{
+    private GameInput _gameInput;
+
+    private void OnEnable()
+    {
+        // Enable object
+        if (_gameInput == null)
+        {
+            _gameInput = new GameInput();
+
+            // Mapping events here to GameInput, wherein input events are triggered with events defined on InputReader.
+            _gameInput.Gameplay.SetCallbacks(this);
+            _gameInput.UI.SetCallbacks(this);
+
+            // Have gameplay as the first mapping to be enabled
+            SetGameplay();
+        }
+    }
+
+    /// <summary>
+    /// Enables gameplay as current state.
+    /// </summary>
+    public void SetGameplay()
+    {
+        _gameInput.Gameplay.Enable();
+        _gameInput.UI.Disable();
+    }
+
+    /// <summary>
+    /// Enables UI as current state.
+    /// </summary>
+    public void SetUI()
+    {
+        _gameInput.UI.Enable();
+        _gameInput.Gameplay.Disable();
+    }
+
+    public event Action<Vector2> MoveEvent;
+    public event Action JumpEvent;
+    public event Action PauseEvent;
+    public event Action ResumeEvent;
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log($"Phase: {context.phase}, Value: {context.ReadValue<Vector2>()}");
+    }
+
+    public void OnResume(InputAction.CallbackContext context)
+    {
+        
+    }
+}
