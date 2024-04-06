f147019ad34d3456b60dd5f5b94a3c65b5577e68
ndrw-ynz <andrewyanza4@gmail.com>
2024-04-06 16:10:28 +0800

    feat: add basic rotation and complete movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,42 +6,60 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private InputReader _input;
-    [SerializeField] private float _movementSpeed = 5.0f;
+    [SerializeField] private float _movementSpeed = 2.5f;
     [SerializeField] private float _moveSmoothTime = 0.1f;
     [SerializeField] private float _jumpSpeed = 6.0f;
     [SerializeField] private float _gravity = 9.81f;
-    [SerializeField] private float _lookSensitivity = 50.0f;
+    [SerializeField] private float _lookSensitivity = 0.5f; 
 
 
     private CharacterController _controller;
     private Vector3 _moveDirection;
     private Vector3 _moveDampVelocity;
     private Vector3 _forceVelocity;
+
+    private Vector2 _lookRotation;
     private bool _isJumping;
+
+    public Transform playerCamera;
 
     void Start()
     {
         // Subscribing the functions on events.
         _input.MoveEvent += HandleMove;
         _input.JumpEvent += HandleJump;
+        _input.LookEvent += HandleLook;
 
         _controller = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
+        Look();
         Move();
         Jump();
     }
 
     private void HandleMove(Vector2 dir)
     {
+        Debug.Log(dir);
         _moveDirection = new Vector3(dir.x, 0f, dir.y);
     }
 
     private void HandleJump()
     {
         _isJumping = _controller.isGrounded;
+    }
+
+    private void HandleLook(Vector2 dir)
+    {
+        _lookRotation.x -= dir.y * _lookSensitivity;
+        _lookRotation.y += dir.x * _lookSensitivity;
+
+        _lookRotation.x = Mathf.Clamp(_lookRotation.x, -90f, 90f);
+        Debug.Log(_lookRotation);
     }
 
     /// <summary>
@@ -51,7 +69,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (_moveDirection == Vector3.zero) return;
 
-        Vector3 moveVector = transform.TransformDirection(_moveDirection);
+        Vector3 moveVector = _moveDirection;
         if (moveVector.magnitude > 1) moveVector.Normalize();
 
         _moveDirection = Vector3.SmoothDamp(
@@ -61,7 +79,7 @@ public class PlayerMovement : MonoBehaviour
             _moveSmoothTime
             );
 
-        _controller.Move(_moveDirection * Time.deltaTime);
+        _controller.Move(transform.TransformDirection(_moveDirection) * _movementSpeed * Time.deltaTime);
     }
     /// <summary>
     /// Jump event triggered every frame in Update.
@@ -82,5 +100,13 @@ public class PlayerMovement : MonoBehaviour
             _forceVelocity.y -= _gravity * Time.deltaTime;
         }
         _controller.Move(_forceVelocity * Time.deltaTime);
+    }
+    /// <summary>
+    /// Look event triggered every frame in Update.
+    /// </summary>
+    private void Look()
+    {
+        transform.eulerAngles = new Vector3(0f, _lookRotation.y, 0f);
+        playerCamera.localEulerAngles = new Vector3(_lookRotation.x, 0f, 0f);
     }
 }
