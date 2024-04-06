88c2e53d58689a5c2018cdf4ecae1b93787ed633
ndrw-ynz <andrewyanza4@gmail.com>
2024-04-06 12:05:12 +0800

    feat: add game manager for initial input handling of pause and resume event

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private InputReader _input;
+    [SerializeField] private GameObject pauseMenu;
+    void Start()
+    {
+        _input.PauseEvent += HandlePause;
+        _input.ResumeEvent += HandleResume;
+    }
+
+    private void HandlePause()
+    {
+        pauseMenu.SetActive(true);
+    }
+
+    private void HandleResume()
+    {
+        pauseMenu.SetActive(false);
+    }
+}
