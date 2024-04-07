a0992ec99e0a0244d2567148324d84b6875b62cf
ndrw-ynz <andrewyanza4@gmail.com>
2024-04-07 21:41:06 +0800

    feat: add initial UI for errors and variance subactivity

--- Assets/Scripts/Activity 1/Sub Activity/Errors.cs
+++ Assets/Scripts/Activity 1/Sub Activity/Errors.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Errors : MonoBehaviour, IInteractable
 {
+    [SerializeField] private InputReader _inputReader;
+    [SerializeField] private GameObject viewErrorsUI;
     public void Interact()
     {
         Debug.Log("Activated errors panel!");
+        _inputReader.SetUI();
+        viewErrorsUI.SetActive(true);
     }
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Activity 1/Sub Activity/Variance.cs
+++ Assets/Scripts/Activity 1/Sub Activity/Variance.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Variance : MonoBehaviour, IInteractable
 {
+    [SerializeField] private InputReader _inputReader;
+    [SerializeField] private GameObject viewVarianceUI;
     public void Interact()
     {
         Debug.Log("Activated variance panel!");
+        _inputReader.SetUI();
+        viewVarianceUI.SetActive(true);
     }
     // Start is called before the first frame update
     void Start()
