50c20ebef95691eae21d77673b16e6fbeaf14dc5
ndrw-ynz <andrewyanza4@gmail.com>
2024-04-07 21:01:03 +0800

    refactor: changed directory of scripts

---
+++ Assets/Scripts/Activity 1/Sub Activity/ScientificNotation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScientificNotation : MonoBehaviour, IInteractable
+{
+    // button from UI needed here, or UI itself
+    [SerializeField] private InputReader _inputReader;
+    [SerializeField] private GameObject viewScientificNotationUI;
+    public void Start()
+    {
+        return;
+    }
+    // need game object for UI.
+    public void Interact()
+    {
+        _inputReader.SetUI();
+        viewScientificNotationUI.SetActive(true);
+    }
+}
