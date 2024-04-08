156f0a207b1e0c20758be0ccf5f061cad1d51d14
ndrw-ynz <andrewyanza4@gmail.com>
2024-04-08 18:06:35 +0800

    feat: add feature for randomly generating values of box containers

--- Assets/Scripts/Activity 1/Sub Activity/BoxContainer.cs
+++ Assets/Scripts/Activity 1/Sub Activity/BoxContainer.cs
@@ -1,17 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UIElements;
 
 public class BoxContainer : IInteractable
 {
+    [SerializeField] private TextMeshPro measurementText;
+    private int _numericalValue;
+    private string _unitOfMeasurement;
+
     public override void Interact()
     {
-        
+
     }
 
-    void Start()
+    public void SetValues(ScientificNotationSO levelData)
     {
-        
-    }
+        _numericalValue = Random.Range(levelData.minimumNumericalValue, levelData.maximumNumericalValue);
+        int randomIndex = Random.Range(0, levelData.unitOfMeasurements.Count);
+        _unitOfMeasurement = levelData.unitOfMeasurements[randomIndex];
 
+        measurementText.text = $"{_numericalValue} {_unitOfMeasurement}";
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,19 +5,32 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private InputReader _input;
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private ScientificNotationSO level1;
+    [SerializeField] private List<BoxContainer> boxContainers;
+
     void Start()
     {
         _input.PauseEvent += HandlePause;
         _input.ResumeEvent += HandleResume;
+
+        // TODO: handle event for randomizing contents of box containers
+        RandomlyGenerateBoxValues();
     }
 
     private void HandlePause()
     {
-        pauseMenu.SetActive(true);
+        //pauseMenu.SetActive(true);
     }
 
     private void HandleResume()
     {
-        pauseMenu.SetActive(false);
+        //pauseMenu.SetActive(false);
+    }
+
+    private void RandomlyGenerateBoxValues()
+    {
+        foreach (BoxContainer box in boxContainers) {
+            box.SetValues(level1);
+        }
     }
 }
