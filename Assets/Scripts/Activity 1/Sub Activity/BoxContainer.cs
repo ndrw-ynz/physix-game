b07c18e19857e869c849b72200896b959f785f50
ndrw-ynz <andrewyanza4@gmail.com>
2024-04-08 11:51:58 +0800

    feat: add BoxContainer for AP subactivity

---
+++ Assets/Scripts/Activity 1/Sub Activity/BoxContainer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxContainer : IInteractable
+{
+    public override void Interact()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+}
