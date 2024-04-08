96824e27d183fa9d40594edd1ee14c2f999a409b
ndrw-ynz <andrewyanza4@gmail.com>
2024-04-08 11:51:00 +0800

    feat: add method for highlighting IInteractable on near field contact

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-interface IInteractable
+public abstract class IInteractable : MonoBehaviour
 {
-    public void Interact();
+    public Material defaultMaterial;
+    public Material highlightMaterial;
+    public abstract void Interact();
 }
 
 public class Interactor : MonoBehaviour
@@ -13,6 +15,8 @@ public class Interactor : MonoBehaviour
     public Transform interactorSource;
     public float interactRange = 3.0f;
 
+    private IInteractable _selectedInteractable;
+
     void Start()
     {
         _input.InteractEvent += HandleInteract;
@@ -20,7 +24,7 @@ public class Interactor : MonoBehaviour
 
     void Update()
     {
-        Interact();
+        HandleHighlight();
     }
 
     private void HandleInteract()
@@ -36,8 +40,23 @@ public class Interactor : MonoBehaviour
         }
     }
 
-    private void Interact()
+    private void HandleHighlight()
     {
+        if (_selectedInteractable != null)
+        {
+            _selectedInteractable.GetComponent<Renderer>().material = _selectedInteractable.defaultMaterial;
+            _selectedInteractable = null;
+        }
 
+        Vector2 screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray r = Camera.main.ScreenPointToRay(screenCenter);
+        if (Physics.Raycast(r, out RaycastHit hit, interactRange))
+        {
+            if (hit.collider.gameObject.TryGetComponent(out IInteractable interactObj))
+            {
+                interactObj.GetComponent<Renderer>().material = interactObj.highlightMaterial;
+                _selectedInteractable = interactObj;
+            } 
+        }
     }
 }
