44406c361b9d305f8fa2af5ef2724864c3658685
ndrw-ynz <andrewyanza4@gmail.com>
2024-05-02 22:05:41 +0800

    feat: add intiial event handling for Operand and Operator inputs

--- Assets/Scripts/Activity 1/UI/DropHandler.cs
+++ Assets/Scripts/Activity 1/UI/DropHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -8,11 +9,18 @@ using UnityEngine.UI;
 public class DropHandler : MonoBehaviour, IDropHandler
 {
 	private TextMeshProUGUI _placeholderText;
+	// todo: to make it easier, make use of bools for checking if operator or operand to avoid regex use
+	private bool _isPrevOperator;
+	private bool _isParentheses;
 
 	public void Start()
 	{
 		_placeholderText = GetComponentInChildren<TextMeshProUGUI>();
 		_placeholderText.text = "";
+
+		OperandButton.OperandButtonEvent += InsertOperand;
+		OperatorButton.OperatorButtonEvent += InsertOperator;
+		_isPrevOperator = true;
 	}
 
 	public void OnDrop(PointerEventData eventData)
@@ -20,5 +28,42 @@ public class DropHandler : MonoBehaviour, IDropHandler
 		Draggable dragObject = eventData.pointerDrag.GetComponent<Draggable>();
 		_placeholderText.text = dragObject.GetValue().ToString();
 	}
+
+	private void InsertOperand(int operand)
+	{
+		_placeholderText.text += operand.ToString();
+		_isPrevOperator = false;
+	}
+
+	private void InsertOperator(string op)
+	{
+		if (!string.IsNullOrEmpty(_placeholderText.text))
+		{
+			char lastChar = _placeholderText.text[_placeholderText.text.Length - 1];
+
+			if (char.IsDigit(lastChar) || lastChar == ')')
+			{
+				// Append the operator directly
+				_placeholderText.text += op;
+			}
+			else if (lastChar == '(' || "+-*/".Contains(lastChar))
+			{
+				if (lastChar == '(')
+				{
+					if (op != "-")
+					{
+						_placeholderText.text += op;
+					}
+				}
+				else
+				{
+					_placeholderText.text = _placeholderText.text.Remove(_placeholderText.text.Length - 1) + op;
+				}
+			}
+		}
+	}
+
+
+
 }
 
