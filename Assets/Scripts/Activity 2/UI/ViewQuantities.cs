b21262f7848829d55b2f1e776a99e4b4e0645d33
ndrw-ynz <andrewyanza4@gmail.com>
2024-06-07 13:23:40 +0800

    feat: create initial script for ViewQuantities and its QuantityAreas for Quantities

---
+++ Assets/Scripts/Activity 2/UI/ViewQuantities.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewQuantities : MonoBehaviour
+{
+	public QuantityArea givenQuantitiesHolder;
+    public QuantityArea scalarQuantitiesHolder;
+    public QuantityArea vectorQuantitiesHolder;
+}
