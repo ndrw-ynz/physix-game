e0c564d8e0779eaec44980e48be34b44ffc9d111
ndrw-ynz <andrewyanza4@gmail.com>
2024-06-14 02:27:30 +0800

    feat: add initial VectorInfo for holding, accessing, and displaying information of Vectors

--- Assets/Scripts/Activity 2/UI/VectorInfo.cs
+++ Assets/Scripts/Activity 2/UI/VectorInfo.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class VectorInfo
 {
-    public int magnitudeValue;
-    public int directionValue;
-    public VectorDisplay vectorDisplay;
-    // compute for x and y component?
+	public int magnitudeValue;
+	public int directionValue;
+	public VectorDisplay vectorDisplay;
+	// compute for x and y component?
 
 	public VectorInfo(int magnitudeValue, int directionValue, VectorDisplay vectorDisplay)
-    {
-        this.magnitudeValue = magnitudeValue;
-        this.directionValue = directionValue;
-        this.vectorDisplay = vectorDisplay;
-        // here compute for x and y component
-    }
+	{
+		this.magnitudeValue = magnitudeValue;
+		this.directionValue = directionValue;
+		this.vectorDisplay = vectorDisplay;
+		// here compute for x and y component
+	}
 }
