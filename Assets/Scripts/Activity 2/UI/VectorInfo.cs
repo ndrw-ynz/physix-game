5449c6413cbe96c4b624d1551b2dd24a6ef62ec1
ndrw-ynz <andrewyanza4@gmail.com>
2024-06-14 16:22:21 +0800

    feat: add DirectionType component for VectorInfo

--- Assets/Scripts/Activity 2/UI/VectorInfo.cs
+++ Assets/Scripts/Activity 2/UI/VectorInfo.cs
@@ -6,13 +6,16 @@ public class VectorInfo
 {
 	public int magnitudeValue;
 	public int directionValue;
+	public DirectionType directionType;
 	public VectorDisplay vectorDisplay;
 	// compute for x and y component?
 
-	public VectorInfo(int magnitudeValue, int directionValue, VectorDisplay vectorDisplay)
+
+	public VectorInfo(int magnitudeValue, int directionValue, DirectionType directionType, VectorDisplay vectorDisplay)
 	{
 		this.magnitudeValue = magnitudeValue;
 		this.directionValue = directionValue;
+		this.directionType = directionType;
 		this.vectorDisplay = vectorDisplay;
 		// here compute for x and y component
 	}
