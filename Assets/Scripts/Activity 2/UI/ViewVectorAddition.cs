c153dc7b0d83fa27cb4b447e012bbc8f52c82c36
ndrw-ynz <andrewyanza4@gmail.com>
2024-06-19 18:19:04 +0800

    feat: add setup for filling x- and y-inputFieldContainers

--- Assets/Scripts/Activity 2/UI/ViewVectorAddition.cs
+++ Assets/Scripts/Activity 2/UI/ViewVectorAddition.cs
@@ -1,5 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,8 +7,12 @@ public class ViewVectorAddition : MonoBehaviour
 {
     [Header("Prefabs")]
     public VectorComponentDisplay vectorComponentDisplayPrefab;
+    public TMP_InputField valueInputFieldPrefab;
+    public TextMeshProUGUI plusSignTextPrefab;
     [Header("Containers")]
     public VerticalLayoutGroup vectorComponentDisplayContainer;
+    public HorizontalLayoutGroup xInputFieldContainer;
+    public HorizontalLayoutGroup yInputFieldContainer;
      public void SetupViewVectorAddition(List<VectorInfo> vectorInfoList)
     {
         // Setting up contents of vectorComponentDisplayContainer
@@ -18,5 +22,24 @@ public class ViewVectorAddition : MonoBehaviour
             vectorComponentDisplay.SetupVectorComponentDisplay(vectorInfo);
             vectorComponentDisplay.transform.SetParent(vectorComponentDisplayContainer.transform, false);
         }
-    }
+
+        // Setting up n-inputFieldContainer
+        SetupInputFieldContainer(xInputFieldContainer, vectorInfoList.Count);
+		SetupInputFieldContainer(yInputFieldContainer, vectorInfoList.Count);
+	}
+
+	private void SetupInputFieldContainer(HorizontalLayoutGroup inputFieldContainer, int numberOfVectors)
+    {
+		for (int i = 0; i < numberOfVectors; i++)
+		{
+			TMP_InputField valueInputField = Instantiate(valueInputFieldPrefab);
+			valueInputField.transform.SetParent(inputFieldContainer.transform, false);
+
+			if (i + 1 < numberOfVectors)
+			{
+				TextMeshProUGUI plusSignText = Instantiate(plusSignTextPrefab);
+				plusSignText.transform.SetParent(inputFieldContainer.transform, false);
+			}
+		}
+	}
 }
