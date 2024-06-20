85d168befac14d6b7cecd5c61f4e1267257a69b3
ndrw-ynz <andrewyanza4@gmail.com>
2024-06-20 17:11:39 +0800

    feat: add utility functions for validating n-component addition, magnitude and direction submission

--- Assets/Scripts/Activity 2/ActivityTwoUtilities.cs
+++ Assets/Scripts/Activity 2/ActivityTwoUtilities.cs
@@ -1,6 +1,9 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public static class ActivityTwoUtilities
 {
@@ -24,5 +27,55 @@ public static class ActivityTwoUtilities
 	{
 		bool canEvaluate = ExpressionEvaluator.Evaluate(submittedText, out float value);
 		return canEvaluate && Mathf.Abs((float)(value - correctValue)) <= 0.0001;
+	}
+
+	public static bool ValidateComponentInputFields(List<float> trueComponentValues, HorizontalLayoutGroup componentContainer)
+	{
+		TMP_InputField[] inputFields = componentContainer.GetComponentsInChildren<TMP_InputField>();
+		List<float> submittedComponentValues = inputFields.Select(field =>
+		{
+			if (float.TryParse(field.text, out float value))
+			{
+				return value;
+			}
+			else
+			{
+				return 0;
+			}
+		}).ToList();
+
+		var trueComponentCounts = trueComponentValues.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+		var submittedComponentCounts = submittedComponentValues.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+
+		return trueComponentCounts.Count == submittedComponentCounts.Count
+			&& !trueComponentCounts.Except(submittedComponentCounts).Any();
+	}
+
+	public static bool ValidateMagnitudeSubmission(float xSum, float ySum, TMP_InputField magnitudeResultField)
+	{
+		// Evaluate magnitude result field
+		bool isMagnitudeResultEvaluated = ExpressionEvaluator.Evaluate(magnitudeResultField.text, out double magnitudeResult);
+		if (isMagnitudeResultEvaluated == false) return false;
+
+		// Evaluate magnitude equation with xSum and ySum
+		bool canEvaluateMagnitude = ExpressionEvaluator.Evaluate($"sqrt(({xSum})^2+({ySum})^2)", out double computationResult);
+		if (canEvaluateMagnitude)
+		{
+			computationResult = Math.Round(computationResult, 4);
+			return Math.Abs(computationResult - magnitudeResult) <= 0.0001;
+		}
+		return false;
+	}
+
+	public static bool ValidateDirectionSubmission(float xSum, float ySum, TMP_InputField directionResultField)
+	{
+		// Evaluate direction result field
+		bool isDirectionResultEvaluated = ExpressionEvaluator.Evaluate(directionResultField.text, out double directionResult);
+		if (isDirectionResultEvaluated == false) return false;
+
+		// Evaluate direction equation with xSum and ySum
+		double computationResult = Math.Atan2(ySum, xSum) * (180 / Math.PI);
+		computationResult = Math.Round(computationResult, 4);
+		return Math.Abs(computationResult - directionResult) <= 0.0001;
 	}
 }
