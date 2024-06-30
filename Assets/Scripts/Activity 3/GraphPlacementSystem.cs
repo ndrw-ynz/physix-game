e19b88d74be532a9cba903f691bde9a81576e069
ndrw-ynz <andrewyanza4@gmail.com>
2024-06-30 21:57:00 +0800

    feat: add GraphPlacementSystem.cs script with logic for displaying indicator on interacting with graph

---
+++ Assets/Scripts/Activity 3/GraphPlacementSystem.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GraphPlacementSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject mouseIndicator;
+	[SerializeField] private GameObject pointIndicator;
+    [SerializeField] private GraphManager graphManager;
+	[SerializeField] private Grid grid;
+
+	private void Update()
+	{
+		if (graphManager.interactiveGraphCamera)
+		{
+			Vector3 mousePosition = graphManager.GetSelectedMapPosition();
+			Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+			mouseIndicator.transform.position = mousePosition;
+			pointIndicator.transform.position = grid.CellToWorld(gridPosition);
+		} else
+		{
+			mouseIndicator.transform.position = new Vector3(0, -1, 0);
+			pointIndicator.transform.position = new Vector3(0, -1, 0);
+		}
+	}
+}
