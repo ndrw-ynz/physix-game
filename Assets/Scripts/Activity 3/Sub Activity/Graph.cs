323d573553f36b6cd18b49b0a290c276a0d9ffb2
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-01 15:58:35 +0800

    feat: add Graph.cs script to contain interactiveGraphCamera, graphGrid, and graphLineRenderer

---
+++ Assets/Scripts/Activity 3/Sub Activity/Graph.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [Header("Interactive Graph Camera")]
+    public Camera interactiveGraphCamera;
+    [Header("Grid")]
+    public Grid graphGrid;
+    [Header("Line Renderer")]
+    public LineRenderer graphLineRenderer;
+}
