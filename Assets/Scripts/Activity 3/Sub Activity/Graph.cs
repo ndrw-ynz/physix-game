089190573b2df75d7a4cc480f08308a3294adfc9
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-01 17:55:32 +0800

    feat: add setup of Graph Line Renderer and logic for updating points on graph

--- Assets/Scripts/Activity 3/Sub Activity/Graph.cs
+++ Assets/Scripts/Activity 3/Sub Activity/Graph.cs
@@ -10,4 +10,25 @@ public class Graph : MonoBehaviour
     public Grid graphGrid;
     [Header("Line Renderer")]
     public LineRenderer graphLineRenderer;
+
+	private void Start()
+	{
+		graphLineRenderer.startWidth = 0.2f;
+		graphLineRenderer.endWidth = 0.2f;
+		graphLineRenderer.positionCount = 9;
+		SetupGraphLineRenderer(9);
+	}
+
+	private void SetupGraphLineRenderer(int numOfPoints)
+	{
+		for (int i = 0; i < numOfPoints; i++)
+		{
+			graphLineRenderer.SetPosition(i, new Vector3(i,0,i));
+		}
+	}
+
+	public void UpdateColumnPointOnGraph(int columnValue, int rowValue)
+	{
+		graphLineRenderer.SetPosition(columnValue, new Vector3(columnValue, 0, rowValue));
+	}
 }
