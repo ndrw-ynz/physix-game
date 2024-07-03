10de90ed8d725a573eaea216f2b97b4990df02fc
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-03 20:37:26 +0800

    feat: add setup SetupGraphs and position, velocity, and acceleration vs time graph values

--- Assets/Scripts/Activity 3/GraphManager.cs
+++ Assets/Scripts/Activity 3/GraphManager.cs
@@ -1,18 +1,44 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using static GraphsSubActivitySO;
+using Random = UnityEngine.Random;
 
 public class GraphManager : MonoBehaviour
 {
     public event Action OnMouseClick;
 
     [Header("Graph")]
+    public Graph positionVsTimeGraph;
+    public Graph velocityVsTimeGraph;
+    public Graph accelerationVsTimeGraph;
     public Graph currentGraph;
     [Header("Layer Mask")]
     [SerializeField] private LayerMask placementLayerMask;
 
     private Vector3 lastMousePosition;
+
+    public void SetupGraphs(GraphsSubActivitySO graphsSO)
+    {
+        foreach (GraphDataset graphDataset in graphsSO.datasets)
+        {
+            string rawStringDataValues = graphDataset.dataset[Random.Range(0, graphDataset.dataset.Count)];
+			List<int> graphPointValues = rawStringDataValues.Split(',').Select(int.Parse).ToList();
+			switch (graphDataset.datasetType)
+            {
+                case DatasetType.Position:
+                    positionVsTimeGraph.InitializeGraph(graphPointValues);
+					break;
+                case DatasetType.Velocity:
+					velocityVsTimeGraph.InitializeGraph(graphPointValues);
+					break;
+                case DatasetType.Acceleration:
+					accelerationVsTimeGraph.InitializeGraph(graphPointValues);
+					break;
+            }
+        }
+    }
 
 	private void Update()
 	{
