baf658688c7921383fcf9e9c9ad07cdecd0b00ab
Powell <powell.payofilin2400@gmail.com>
2024-07-06 23:40:47 +0800

    added textmeshpro to sector button class note: only temporary, will make it better

--- Assets/Scripts/Topic Discussion 1/DiscussionPrevNextSectorButton.cs
+++ Assets/Scripts/Topic Discussion 1/DiscussionPrevNextSectorButton.cs
@@ -7,9 +7,13 @@ public class DiscussionPrevNextSectorButton : MonoBehaviour
 {
     public DiscussionNavigator discussionNavigator;
     public string action;
+    public TextMeshProUGUI prevSectorTitle;
+    public TextMeshProUGUI nextSectorTitle;
 
     public void OnClick()
     {
         discussionNavigator.changeSector(action);
+        prevSectorTitle.SetText(discussionNavigator.setPrevSectorText());
+        nextSectorTitle.SetText(discussionNavigator.setNextSectorText());
     }
 }
