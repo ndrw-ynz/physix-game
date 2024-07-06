d2e5413903dbb7c83f69c9c378f0ea163e993aea
Powell <powell.payofilin2400@gmail.com>
2024-07-06 23:31:44 +0800

    feat: added textmeshpro to button class note: only temporary, will make it better

--- Assets/Scripts/Topic Discussion 1/DiscussionPrevNextPageButton.cs
+++ Assets/Scripts/Topic Discussion 1/DiscussionPrevNextPageButton.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DiscussionPrevNextPageButton : MonoBehaviour
 {
     public DiscussionNavigator discussionNavigator;
     public int step;
+    public TextMeshProUGUI prevSectorTitle;
+    public TextMeshProUGUI nextSectorTitle;
+    
 
     public void OnClick()
     {
         discussionNavigator.changePage(step);
+        prevSectorTitle.SetText(discussionNavigator.setPrevSectorText());
+        nextSectorTitle.SetText(discussionNavigator.setNextSectorText());
     }
-
 }
