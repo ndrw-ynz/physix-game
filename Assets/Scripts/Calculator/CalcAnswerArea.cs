01916f1283f4b790c2ac1ebfa438ff4e1a7d8db4
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-07 16:25:13 +0800

    feat: add script for Calcualtor Answer Area and handling OnDrop functionality

---
+++ Assets/Scripts/Calculator/CalcAnswerArea.cs
@@ -0,0 +1,16 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CalcAnswerArea : MonoBehaviour, IDropHandler
+{
+	[SerializeField] private TextMeshProUGUI answerText;
+	public float answerValue;
+
+	public void OnDrop(PointerEventData eventData)
+	{
+		CalcCompResult compResult = eventData.pointerDrag.GetComponent<CalcCompResult>();
+		answerValue = compResult.result;
+		answerText.text = answerValue.ToString();
+	}
+}
