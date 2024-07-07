e1387d7cc0a85eca136df0fc1bcdedec7f7dd7af
Powell <powell.payofilin2400@gmail.com>
2024-07-08 01:30:25 +0800

    feat: add comprehension button

---
+++ Assets/Scripts/Topic Discussion 1/ComprehensionButton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComprehensionButton : MonoBehaviour
+{
+    public DiscussionNavigator discussionNavigator;
+    public ProgressManager progressManager;
+    public bool flag;
+
+    public void OnClick()
+    {
+        discussionNavigator.changeComprehensionMark(flag);
+        progressManager.loadProgressBars();
+        Debug.Log("Loaded Progress Bars");
+    }
+
+    
+}
