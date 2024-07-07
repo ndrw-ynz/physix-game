d020a05b94cfb0e61ebf4abe151853c3ad4fde5a
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-07 18:16:51 +0800

    feat: add given text and its initialization function, answer area, and accelerate button of View 1D Kinematics UI

--- Assets/Scripts/Activity 3/UI/View1DKinematics.cs
+++ Assets/Scripts/Activity 3/UI/View1DKinematics.cs
@@ -1,15 +1,22 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class View1DKinematics : MonoBehaviour
 {
+	[Header("Given Text")]
+	[SerializeField] private TextMeshProUGUI accelerationGivenText;
     [Header("Prefabs")]
 	[SerializeField] private CalcCompResult compResultPrefab;
     [Header("Input Field")]
     [SerializeField] private TMP_InputField inputField;
+	[Header("Answer Area")]
+	[SerializeField] private CalcAnswerArea answerArea;
 	[Header("Layout Holders")]
 	[SerializeField] private VerticalLayoutGroup compResultHolder;
+	[Header("Buttons")]
+	[SerializeField] private Button accelerateButton;
 
 	private void OnEnable()
 	{
@@ -37,5 +44,10 @@ public class View1DKinematics : MonoBehaviour
 		{
 			// TODO: popup message for invalid computation.
 		}
+	}
+
+	public void SetupGivenText(float initialVelocity, float finalVelocity, float totalTime)
+	{
+		accelerationGivenText.text = $"Initial Velocity = {initialVelocity} km/hr Final Velocity = {finalVelocity} km/hr Time = {totalTime} minutes";
 	}
 }
