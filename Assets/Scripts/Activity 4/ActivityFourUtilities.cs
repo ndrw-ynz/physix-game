31c67d95c60349711b33a4cc183aeea62ce26bc2
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-08 23:50:37 +0800

    chore: add Debug.Log statements for informing computed and submitted maximum height values

--- Assets/Scripts/Activity 4/ActivityFourUtilities.cs
+++ Assets/Scripts/Activity 4/ActivityFourUtilities.cs
@@ -5,6 +5,8 @@ public static class ActivityFourUtilities
 	public static bool ValidateMaximumHeightSubmission(float submittedMaximumHeight, float projectileInitialVelocity, float projectileAngle)
 	{
 		ExpressionEvaluator.Evaluate($"-({projectileInitialVelocity})^2*sin({projectileAngle}*(pi/180))/(2*-9.81)", out float computationResult);
+		Debug.Log($"computed result: {computationResult}");
+		Debug.Log($"submitted : {submittedMaximumHeight}");
 		return Mathf.Abs(submittedMaximumHeight - computationResult) <= 0.0001;
 	}
 }
