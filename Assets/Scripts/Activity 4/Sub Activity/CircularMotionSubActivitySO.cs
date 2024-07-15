436623455ee1b1782ed5f78381aaa7d357b31ff8
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-15 16:32:00 +0800

    refactor: change components of CircularMotionSubActivitySO with radius and time value range

--- Assets/Scripts/Activity 4/Sub Activity/CircularMotionSubActivitySO.cs
+++ Assets/Scripts/Activity 4/Sub Activity/CircularMotionSubActivitySO.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Level Data/CircularMotionSubActivitySO")]
 public class CircularMotionSubActivitySO : ScriptableObject
 {
-	[Header("Velocity Value Range")]
-	public int minimumVelocityValue;
-	public int maximumVelocityValue;
 	[Header("Radius Value Range")]
 	public int minimumRadiusValue;
 	public int maximumRadiusValue;
+	[Header("Time Value Range")]
+	public int minimumTimeValue;
+	public int maximumTimeValue;
 }
