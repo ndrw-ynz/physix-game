30af3f30dc1d131d76ec92d023bf4a145c98d54d
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-15 16:44:34 +0800

    refactor: rename to Time Period Value Range for time in CircularMotionSubActivitySO

--- Assets/Scripts/Activity 4/Sub Activity/CircularMotionSubActivitySO.cs
+++ Assets/Scripts/Activity 4/Sub Activity/CircularMotionSubActivitySO.cs
@@ -6,7 +6,7 @@ public class CircularMotionSubActivitySO : ScriptableObject
 	[Header("Radius Value Range")]
 	public int minimumRadiusValue;
 	public int maximumRadiusValue;
-	[Header("Time Value Range")]
-	public int minimumTimeValue;
-	public int maximumTimeValue;
+	[Header("Time Period Value Range")]
+	public int minimumTimePeriodValue;
+	public int maximumTimePeriodValue;
 }
