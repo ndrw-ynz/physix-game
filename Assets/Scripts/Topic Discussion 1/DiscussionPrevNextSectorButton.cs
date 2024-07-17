79516320980b4197bdb8e9a2408fae2fd38f2fc7
Powell <powell.payofilin2400@gmail.com>
2024-07-17 22:50:08 +0800

    feat: added event for PrevNextSectorClick

--- Assets/Scripts/Topic Discussion 1/DiscussionPrevNextSectorButton.cs
+++ Assets/Scripts/Topic Discussion 1/DiscussionPrevNextSectorButton.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiscussionPrevNextSectorButton : MonoBehaviour
 {
@@ -9,8 +11,12 @@ public class DiscussionPrevNextSectorButton : MonoBehaviour
     public string action;
     public TextMeshProUGUI sectorButtonText;
 
-    public void OnClick()
+    public static event Action<string> PrevNextSectorClickEvent;
+    private Button _prevNextSectorButton;
+
+    private void OnEnable()
     {
-        discussionNavigator.changeSector(action);
+        _prevNextSectorButton = this.GetComponent<Button>();
+        _prevNextSectorButton.onClick.AddListener(() => PrevNextSectorClickEvent.Invoke(action));
     }
 }
