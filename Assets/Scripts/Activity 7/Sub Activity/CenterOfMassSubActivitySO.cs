8db0395b03c5c326a23d117ecc8e48656a514298
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-21 13:06:54 +0800

    feat: add Number of Masses in SO

--- Assets/Scripts/Activity 7/Sub Activity/CenterOfMassSubActivitySO.cs
+++ Assets/Scripts/Activity 7/Sub Activity/CenterOfMassSubActivitySO.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Level Data/CenterOfMass")]
 public class CenterOfMassSubActivitySO : ScriptableObject
 {
+	[Header("Number of Masses")]
+	[Range(3,5)]
+	public int numberOfMasses;
 	[Header("Coordinate Distance Threshold")]
 	public int coordThreshold;
 	[Header("Mass Value Range")]
