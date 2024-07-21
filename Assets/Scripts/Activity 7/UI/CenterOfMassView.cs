b61a053e468c800feb6b8c1976bdd94ebb0b6855
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-21 15:36:43 +0800

    feat: add functionality for display setup of given mass coordinate components in CenterOfMassView UI

--- Assets/Scripts/Activity 7/UI/CenterOfMassView.cs
+++ Assets/Scripts/Activity 7/UI/CenterOfMassView.cs
@@ -1,16 +1,34 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CenterOfMassView : MonoBehaviour
 {
+	[Header("Layout Containers")]
+	[SerializeField] private VerticalLayoutGroup massCoordinateComponentContainer;
+	[Header("Prefabs")]
+	[SerializeField] private MassCoordinateComponent massCoordinateComponentPrefab;
 	[Header("Calculation Displays")]
-	[SerializeField] GameObject MassTimesXCoords;
-	[SerializeField] GameObject MassTimesYCoords;
-	[SerializeField] GameObject CenterOfMassXDir;
-	[SerializeField] GameObject CenterOfMassYDir;
+	[SerializeField] private GameObject MassTimesXCoords;
+	[SerializeField] private GameObject MassTimesYCoords;
+	[SerializeField] private GameObject CenterOfMassXDir;
+	[SerializeField] private GameObject CenterOfMassYDir;
 	[Header("Interactive Buttons")]
-    [SerializeField] Button leftPageButton;
-	[SerializeField] Button rightPageButton;
+    [SerializeField] private Button leftPageButton;
+	[SerializeField] private Button rightPageButton;
+
+	#region View Setup
+	public void SetupCenterOfMassView(List<MassCoordinatePair> massCoordinatePairs)
+	{
+		// Setting up mass coordinate components for UI display of given values
+		for (int i = 0; i < massCoordinatePairs.Count; i++)
+		{
+			MassCoordinateComponent massCoordinate = Instantiate(massCoordinateComponentPrefab);
+			massCoordinate.SetupInputFields(massCoordinatePairs[i], i + 1);
+			massCoordinate.transform.SetParent(massCoordinateComponentContainer.transform, false);
+		}
+	}
+	#endregion
 
 	#region Buttons
 	public void OnLeftPageButtonClick()
