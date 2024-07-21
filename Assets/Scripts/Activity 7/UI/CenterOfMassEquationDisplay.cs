4a76356c0a6fac38759057221c2354388430ef4d
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-21 19:34:47 +0800

    feat: add CenterOfMassEquationDisplay UI script and functionality for changing result based on numerator and denominator changes

---
+++ Assets/Scripts/Activity 7/UI/CenterOfMassEquationDisplay.cs
@@ -0,0 +1,31 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class CenterOfMassEquationDisplay : MonoBehaviour
+{
+    [Header("Input Fields")]
+    [SerializeField] private TMP_InputField numeratorInputField;
+    [SerializeField] private TMP_InputField denominatorInputField;
+    [Header("Result Field")]
+    [SerializeField] private TMP_InputField centerOfMassResultInputField;
+
+	public void OnInputFieldChange()
+	{
+		string numerator = numeratorInputField.text;
+		if (string.IsNullOrEmpty(numerator)) numerator = "0";
+		string denominator = denominatorInputField.text;
+		if (string.IsNullOrEmpty(denominator)) denominator = "0";
+
+		bool canEvaluate = ExpressionEvaluator.Evaluate($"{numerator}/{denominator}", out float result);
+		result = (float) Math.Round(result, 2);
+		if (canEvaluate)
+		{
+			centerOfMassResultInputField.text = $"{result}";
+		}
+		else
+		{
+			centerOfMassResultInputField.text = "N/A";
+		}
+	}
+}
