0044e3427edcf4751449dd0f68f62202f72737d3
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-22 16:49:44 +0800

    feat: add CenterOfMassAnswerSubmission class for submission of answers in OnSubmitButtonClick with SubmitAnswerEvent action event

--- Assets/Scripts/Activity 7/UI/CenterOfMassView.cs
+++ Assets/Scripts/Activity 7/UI/CenterOfMassView.cs
@@ -1,38 +1,98 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class CenterOfMassAnswerSubmission
+{
+	public int?[] massTimesXCoordinates { get; private set; }
+	public int?[] massTimesYCoordinates { get; private set; }
+	public int? sumOfMassTimesXCoordinates { get; private set; }
+	public int? sumOfMassTimesYCoordinates { get; private set; }
+	public int? totalMassX { get; private set; }
+	public int? totalMassY { get; private set; }
+	public float? centerOfMassX { get; private set; }
+	public float? centerOfMassY { get; private set; }
+
+	public void SetMassTimesCoordinates(MassCoordinateProductDisplay[] xMassCoordinateProductDisplays, MassCoordinateProductDisplay[] yMassCoordinateProductDisplays)
+	{
+		massTimesXCoordinates = new int?[xMassCoordinateProductDisplays.Length];
+		for (int i = 0; i < xMassCoordinateProductDisplays.Length; i++)
+		{
+			massTimesXCoordinates[i] = xMassCoordinateProductDisplays[i].productValue;
+		}
+
+		massTimesYCoordinates = new int?[yMassCoordinateProductDisplays.Length];
+		for (int i = 0; i < yMassCoordinateProductDisplays.Length; i++)
+		{
+			massTimesYCoordinates[i] = yMassCoordinateProductDisplays[i].productValue;
+		}
+	}
+
+	public void SetSumOfMassTimesCoordinates(int sumOfMassTimesXCoordinates, int sumOfMassTimesYCoordinates)
+	{
+		this.sumOfMassTimesXCoordinates = sumOfMassTimesXCoordinates;
+		this.sumOfMassTimesYCoordinates = sumOfMassTimesYCoordinates;
+	}
+
+	public void SetTotalMass(int totalMassX, int totalMassY)
+	{
+		this.totalMassX = totalMassX;
+		this.totalMassY = totalMassY;
+	}
+
+	public void SetCenterOfMass(float? centerOfMassX, float? centerOfMassY)
+	{
+		this.centerOfMassX = centerOfMassX;
+		this.centerOfMassY = centerOfMassY;
+	}
+}
+
 public class CenterOfMassView : MonoBehaviour
 {
+	public static event Action<CenterOfMassAnswerSubmission> SubmitAnswerEvent;
+
 	[Header("Graph Coordinate Plotter")]
 	[SerializeField] private GraphCoordinatePlotter graphCoordinatePlotter;
+
 	[Header("Mass Coordinate Component Container")]
 	[SerializeField] private VerticalLayoutGroup massCoordinateComponentContainer;
+
 	[Header("Mass Coordinate Product Containers")]
 	[SerializeField] private HorizontalLayoutGroup XMassCoordinateProductContainer;
 	[SerializeField] private HorizontalLayoutGroup YMassCoordinateProductContainer;
+
 	[Header("Sum of Mass Coordinate Product Containers")]
 	[SerializeField] private HorizontalLayoutGroup XSumOfMassCoordinateProductContainer;
 	[SerializeField] private HorizontalLayoutGroup YSumOfMassCoordinateProductContainer;
+
 	[Header("Sum of Mass Containers")]
 	[SerializeField] private HorizontalLayoutGroup XSumOfMassContainer;
 	[SerializeField] private HorizontalLayoutGroup YSumOfMassContainer;
+
 	[Header("Sum Result Fields")]
 	[SerializeField] private TMP_InputField XMassCoordinateProductSumResultField;
 	[SerializeField] private TMP_InputField YMassCoordinateProductSumResultField;
 	[SerializeField] private TMP_InputField XSumOfMassResultField;
 	[SerializeField] private TMP_InputField YSumOfMassResultField;
+
 	[Header("Prefabs")]
 	[SerializeField] private MassCoordinateComponentDisplay massCoordinateComponentDisplayPrefab;
 	[SerializeField] private MassCoordinateProductDisplay massCoordinateProductDisplayPrefab;
 	[SerializeField] private TMP_InputField numberInputFieldPrefab;
 	[SerializeField] private TextMeshProUGUI plusSignTextPrefab;
+
 	[Header("Calculation Displays")]
 	[SerializeField] private GameObject massTimesXCoords;
 	[SerializeField] private GameObject massTimesYCoords;
 	[SerializeField] private GameObject centerOfMassXDir;
 	[SerializeField] private GameObject centerOfMassYDir;
+
+	[Header("Equation Displays")]
+	[SerializeField] private CenterOfMassEquationDisplay xCenterOfMassEquationDisplay;
+	[SerializeField] private CenterOfMassEquationDisplay yCenterOfMassEquationDisplay;
+
 	[Header("Interactive Buttons")]
     [SerializeField] private Button leftPageButton;
 	[SerializeField] private Button rightPageButton;
@@ -128,6 +188,33 @@ public class CenterOfMassView : MonoBehaviour
 
 		massTimesYCoords.gameObject.SetActive(true);
 		centerOfMassYDir.gameObject.SetActive(true);
+	}
+
+	public void OnSubmitButtonClick()
+	{
+		CenterOfMassAnswerSubmission centerOfMassAnswerSubmission = new CenterOfMassAnswerSubmission();
+
+		// Set MassTimesCoordinates for X and Y
+		MassCoordinateProductDisplay[] XMassCoordinateProductDisplays = XMassCoordinateProductContainer.GetComponentsInChildren<MassCoordinateProductDisplay>();
+		MassCoordinateProductDisplay[] YMassCoordinateProductDisplays = YMassCoordinateProductContainer.GetComponentsInChildren<MassCoordinateProductDisplay>();
+		centerOfMassAnswerSubmission.SetMassTimesCoordinates(XMassCoordinateProductDisplays, YMassCoordinateProductDisplays);
+
+		// Set SumOfMassTimesCoordinates for X and Y
+		int SumOfMassTimesXCoordinates = int.Parse(XMassCoordinateProductSumResultField.text);
+		int SumOfMassTimesYCoordinates = int.Parse(YMassCoordinateProductSumResultField.text);
+		centerOfMassAnswerSubmission.SetSumOfMassTimesCoordinates(SumOfMassTimesXCoordinates, SumOfMassTimesYCoordinates);
+
+		// Set Total Mass for X and Y
+		int totalMassX = int.Parse(XSumOfMassResultField.text);
+		int totalMassY = int.Parse(YSumOfMassResultField.text);
+		centerOfMassAnswerSubmission.SetTotalMass(totalMassX, totalMassY);
+
+		// Set Center of Mass for X and Y
+		float? centerOfMassX = xCenterOfMassEquationDisplay.centerOfMassValue;
+		float? centerOfMassY = yCenterOfMassEquationDisplay.centerOfMassValue;
+		centerOfMassAnswerSubmission.SetCenterOfMass(centerOfMassX, centerOfMassY);
+
+		SubmitAnswerEvent?.Invoke(centerOfMassAnswerSubmission);
 	}
 	#endregion
 }
