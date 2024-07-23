3598cd7773d05d075842a9455cbefe5ec7678b44
Powell <powell.payofilin2400@gmail.com>
2024-07-23 23:52:39 +0800

    feat: added hover effect with no animation to page circle buttons

--- Assets/Scripts/Topic Discussion 1/PageCircleButton.cs
+++ Assets/Scripts/Topic Discussion 1/PageCircleButton.cs
@@ -7,10 +7,11 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class PageCircleButton : MonoBehaviour
+public class PageCircleButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     Image[] images;
     public Image buttonOutline;
+    public Image buttonHoverOutline;
     public Image buttonColor;
     public Vector2 startPosition;
     public int pageIndex;
@@ -24,17 +25,38 @@ public class PageCircleButton : MonoBehaviour
         images = GetComponentsInChildren<Image>();
 
         buttonOutline = images[0];
-        buttonColor = images[2];
+        buttonHoverOutline = images[1];
+        buttonColor = images[3];
         startPosition = buttonColor.transform.position;
         pageIndex = index;
         _buttonRenderer = GetComponentInChildren<SpriteRenderer>();
 
         _pageCircleButton = GetComponentInChildren<Button>();
-        _pageCircleButton.onClick.AddListener(() => OnPageCircleClick?.Invoke(pageIndex));
+        _pageCircleButton.onClick.AddListener(() => OnPageCircleButtonCLick());
     }
 
     private void OnDisable()
     {
         _pageCircleButton.onClick.RemoveAllListeners();
+    }
+
+    private void OnPageCircleButtonCLick()
+    {
+        buttonHoverOutline.gameObject.SetActive(false);
+        OnPageCircleClick?.Invoke(pageIndex);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(!buttonOutline.gameObject.activeSelf)
+        {
+            buttonHoverOutline.gameObject.SetActive(true);
+            buttonHoverOutline.color = new Color(1f, 1f, 1f, 0.3f);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonHoverOutline.gameObject.SetActive(false);
     }
 }
