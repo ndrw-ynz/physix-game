1072d7890c3ff106916ff8fe7836efa258e38bd1
Powell <powell.payofilin2400@gmail.com>
2024-07-24 17:18:37 +0800

    feat: add animation for prev next buttons

--- Assets/Scripts/Topic Discussion 1/DiscussionPrevNextPageButton.cs
+++ Assets/Scripts/Topic Discussion 1/DiscussionPrevNextPageButton.cs
@@ -11,12 +11,15 @@ public class DiscussionPrevNextPageButton : MonoBehaviour
     public int step;
     public TextMeshProUGUI prevSectorTitle;
     public TextMeshProUGUI nextSectorTitle;
-    
+    public CanvasGroup canvasGroup;
     public static event Action<int> PrevNextPageClickEvent;
+
     private Button _prevNextPageButton;
 
     private void OnEnable()
     {
+        canvasGroup = this.GetComponent<CanvasGroup>();
+
         _prevNextPageButton = this.GetComponent<Button>();
         _prevNextPageButton.onClick.AddListener(() => PrevNextPageClickEvent?.Invoke(step));
     }
