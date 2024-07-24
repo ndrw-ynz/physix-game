a9ffdf27c24caf3508cb60c0264a62c9b2b4a508
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-24 19:25:20 +0800

    feat: modify interaction system to display interactionUI with interaction text from objects

--- Assets/Scripts/Interactions/InteractionHandler.cs
+++ Assets/Scripts/Interactions/InteractionHandler.cs
@@ -1,8 +1,17 @@
+using TMPro;
 using UnityEngine;
 
 public abstract class IInteractableObject : MonoBehaviour
 {
+	public bool isInteractable { get; private set; }
 	public abstract void Interact();
+
+	public abstract string GetInteractionDescription();
+
+	public void SetInteractable(bool isInteractable)
+	{
+		this.isInteractable = isInteractable;
+	}
 }
 
 public class InteractionHandler : MonoBehaviour
@@ -11,11 +20,36 @@ public class InteractionHandler : MonoBehaviour
 	public Transform interactorSource;
 	public float interactRange = 3.0f;
 
+	public GameObject interactionUI;
+	public TextMeshProUGUI interactionText;
+
 	private IInteractable _selectedInteractable;
 
 	void Start()
 	{
 		_input.InteractEvent += HandleInteract;
+	}
+
+	private void Update()
+	{
+		// Update for displaying interactionUI
+		Vector2 screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
+		Ray r = Camera.main.ScreenPointToRay(screenCenter);
+
+		bool hitInteractableObject = false;
+
+		if (Physics.Raycast(r, out RaycastHit hit, interactRange))
+		{
+			IInteractableObject interactableObject = hit.collider.GetComponent<IInteractableObject>();
+
+			if (interactableObject != null && interactableObject.isInteractable == true)
+			{
+				hitInteractableObject = true;
+				interactionText.text = interactableObject.GetInteractionDescription();
+			}
+		}
+
+		interactionUI.gameObject.SetActive(hitInteractableObject);
 	}
 
 	private void HandleInteract()
@@ -26,7 +60,7 @@ public class InteractionHandler : MonoBehaviour
 		{
 			if (hit.collider.gameObject.TryGetComponent(out IInteractableObject interactObj))
 			{
-				interactObj.Interact();
+				if (interactObj.isInteractable == true) interactObj.Interact();
 			}
 		}
 	}
