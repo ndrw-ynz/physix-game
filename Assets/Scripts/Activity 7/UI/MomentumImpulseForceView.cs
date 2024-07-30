078f0245409cdfc0e1f17764cafc530b1a41712c
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-30 16:55:43 +0800

    feat: add dynamic change to given and calculation displays on momentum-impulse force view based on difficulty for activity seven

--- Assets/Scripts/Activity 7/UI/MomentumImpulseForceView.cs
+++ Assets/Scripts/Activity 7/UI/MomentumImpulseForceView.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class MomentumImpulseForceView : MonoBehaviour
 {
+	[Header("Holders")]
+	[SerializeField] private GameObject easyGivenHolder;
+	[SerializeField] private GameObject mediumHardGivenHolder;
+	[SerializeField] private GameObject easyCalculationHolder;
+	[SerializeField] private GameObject mediumHardCalculationHolder;
+
 	[Header("Given Fields")]
 	[SerializeField] private TMP_InputField givenMassField;
-	[SerializeField] private TMP_InputField givenInitialVelocityField;
-	[SerializeField] private TMP_InputField givenFinalVelocityField;
+	[SerializeField] private TMP_InputField givenDeltaVelocityField; // Easy config
+	[SerializeField] private TMP_InputField givenInitialVelocityField; // Medium-hard config
+	[SerializeField] private TMP_InputField givenFinalVelocityField; // Medium-hard config
 	[SerializeField] private TMP_InputField givenTotalTimeField;
 
 	[Header("Result Fields")]
-	[SerializeField] private TMP_InputField initialMomentumResultField;
-	[SerializeField] private TMP_InputField finalMomentumResultField;
-	[SerializeField] private TMP_InputField changeInMomentumResultField;
+	[SerializeField] private TMP_InputField changeInMomentumResultField; // Easy config (mass * net velocity)
+	[SerializeField] private TMP_InputField initialMomentumResultField; // Medium-hard config
+	[SerializeField] private TMP_InputField finalMomentumResultField; // Medium-hard config
+	[SerializeField] private TMP_InputField changeInMomentumResultField2; // Medium-hard config (finalMomentum - initialMomentum)
 	[SerializeField] private TMP_InputField impulseResultField;
 	[SerializeField] private TMP_InputField netForceResultField;
 
@@ -21,12 +29,42 @@ public class MomentumImpulseForceView : MonoBehaviour
 
 	public void SetupMomentumImpulseForceView(Dictionary<string, float> momentumImpulseForceGivenData)
 	{
-		givenMassField.text = $"{momentumImpulseForceGivenData["mass"]} kg";
-		givenInitialVelocityField.text = $"{momentumImpulseForceGivenData["initialVelocity"]} m/s";
-		givenFinalVelocityField.text = $"{momentumImpulseForceGivenData["finalVelocity"]} m/s";
-		givenTotalTimeField.text = $"{momentumImpulseForceGivenData["totalTime"]} s";
+		// Setting given fields text and displaying UI
+		switch (ActivitySevenManager.difficultyConfiguration)
+		{
+			case Difficulty.Easy:
+				givenDeltaVelocityField.text = $"{momentumImpulseForceGivenData["deltaVelocity"]} m/s";
+				givenMassField.text = $"{momentumImpulseForceGivenData["mass"]} kg";
+				givenTotalTimeField.text = $"{momentumImpulseForceGivenData["totalTime"]} s";
+
+				ShowUIForDifficulty(true);
+				break;
+			case Difficulty.Medium:
+				SetGivenFields(momentumImpulseForceGivenData, "kg", "m/s", "s");
+				ShowUIForDifficulty(false);
+				break;
+			case Difficulty.Hard:
+				SetGivenFields(momentumImpulseForceGivenData, "g", "km/s", "ms", 1000, 0.0001f, 1000);
+				ShowUIForDifficulty(false);
+				break;
+		}
 	}
 
+	private void SetGivenFields(Dictionary<string, float> data, string massUnit, string velocityUnit, string timeUnit, float massMultiplier = 1, float velocityMultiplier = 1, float timeMultiplier = 1)
+	{
+		givenMassField.text = $"{data["mass"] * massMultiplier} {massUnit}";
+		givenInitialVelocityField.text = $"{data["initialVelocity"] * velocityMultiplier} {velocityUnit}";
+		givenFinalVelocityField.text = $"{data["finalVelocity"] * velocityMultiplier} {velocityUnit}";
+		givenTotalTimeField.text = $"{data["totalTime"] * timeMultiplier} {timeUnit}";
+	}
+
+	private void ShowUIForDifficulty(bool isEasy)
+	{
+		easyGivenHolder.gameObject.SetActive(isEasy);
+		mediumHardGivenHolder.gameObject.SetActive(!isEasy);
+		easyCalculationHolder.gameObject.SetActive(isEasy);
+		mediumHardCalculationHolder.gameObject.SetActive(!isEasy);
+	}
 	#endregion
 
 	private void OnSubmitButtonClick()
