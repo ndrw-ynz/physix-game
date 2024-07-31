7bbefaa84058899c9f4dba1edcc64a66db8f6147
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-31 15:47:19 +0800

    feat: add numberOfTests to SO with range 1 to 3

--- Assets/Scripts/Activity 7/Sub Activity/CenterOfMassSubActivitySO.cs
+++ Assets/Scripts/Activity 7/Sub Activity/CenterOfMassSubActivitySO.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Level Data/CenterOfMass")]
 public class CenterOfMassSubActivitySO : ScriptableObject
 {
+	[Header("Number of Tests")]
+	[Range(1, 3)]
+	public int numberOfTests;
 	[Header("Number of Masses")]
 	[Range(3,5)]
 	public int numberOfMasses;
