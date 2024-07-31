7e757dc079a09b0aaf7bfd67e121bcfb6673a8cf
Powell <powell.payofilin2400@gmail.com>
2024-07-31 14:36:47 +0800

    refactor: refactor progress bar button

--- Assets/Scripts/Topic Discussion 1/ProgressBarButton.cs
+++ Assets/Scripts/Topic Discussion 1/ProgressBarButton.cs
@@ -1,43 +1,30 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ProgressBarButton : MonoBehaviour
 {
-    TextMeshProUGUI[] textComponents;
-    Image[] imageComponents;
+    public static event Action<int> ProgressBarClickEvent;
 
     public Image progressBarTempColor;
     public Image progressBarFinalColor;
-    public Vector2 startPosition;
     public TextMeshProUGUI sectorTitleText;
     public TextMeshProUGUI progressCountText;
 
-    public static event Action<int> ProgressBarClickEvent;
-
+    private Vector2 _startPosition;
     private int _sectorIndex;
-    private Button _progressBarButton;
+    [SerializeField] private Button _progressBarButton;
 
 
     public void Initialize(string sectorTitle, string progressCount, int index)
     {
-        textComponents = GetComponentsInChildren<TextMeshProUGUI>();
-        imageComponents = GetComponentsInChildren<Image>();
-
-        progressBarTempColor = imageComponents[0];
-        progressBarFinalColor = imageComponents[1];
-        startPosition = progressBarFinalColor.transform.position;
         
-        sectorTitleText = textComponents[0];
         sectorTitleText.text = sectorTitle;
-        progressCountText = textComponents[1];
         progressCountText.text = progressCount;
+
+        _startPosition = progressBarFinalColor.transform.position;
         _sectorIndex = index;
-        _progressBarButton = this.GetComponent<Button>();
         _progressBarButton.onClick.AddListener(() => ProgressBarClickEvent?.Invoke(_sectorIndex));
     }
 }
