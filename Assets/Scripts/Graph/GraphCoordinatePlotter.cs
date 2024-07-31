e4c800302b8e7b090ba76e78e5a1f6281b0d65f9
ndrw-ynz <andrewyanza4@gmail.com>
2024-07-31 15:46:31 +0800

    feat: add functionality for removing all points in graph

--- Assets/Scripts/Graph/GraphCoordinatePlotter.cs
+++ Assets/Scripts/Graph/GraphCoordinatePlotter.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GraphCoordinatePlotter : MonoBehaviour
@@ -5,10 +6,22 @@ public class GraphCoordinatePlotter : MonoBehaviour
     [Header("Coordinate Point Prefab")]
     [SerializeField] private GameObject coordinatePointPrefab;
 
-    public void PlacePoint(Vector2 coordinate)
+    private List<GameObject> coordinatePointsList = new List<GameObject>();
+
+	public void PlacePoint(Vector2 coordinate)
     {
         GameObject point = Instantiate(coordinatePointPrefab);
         point.transform.position = new Vector3(coordinate.x, 0, coordinate.y);
         point.transform.SetParent(transform, false);
+
+        coordinatePointsList.Add(point);
+    }
+
+    public void RemoveAllPoints()
+    {
+        for (int i = 0; i < coordinatePointsList.Count; i++)
+        {
+            Destroy(coordinatePointsList[i].gameObject);
+        }
     }
 }   
