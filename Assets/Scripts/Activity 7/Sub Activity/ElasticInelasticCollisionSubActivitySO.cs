60aa4e1369a2c77000e27a7ca8b389b12521810a
ndrw-ynz <andrewyanza4@gmail.com>
2024-08-02 18:32:30 +0800

    feat: add script for generating level data for Elastic Inelastic Collision subactivity

---
+++ Assets/Scripts/Activity 7/Sub Activity/ElasticInelasticCollisionSubActivitySO.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Level Data/ElasticInelasticCollisionSO")]
+public class ElasticInelasticCollisionSubActivitySO : ScriptableObject
+{
+	[Header("Number of Tests")]
+	[Range(1, 3)]
+	public int numberOfTests;
+	[Header("Mass Value Range")]
+	public int massMinVal;
+	public int massMaxVal;
+	[Header("Velocity Value Range")]
+	public int velocityMinVal;
+	public int velocityMaxVal;
+}
