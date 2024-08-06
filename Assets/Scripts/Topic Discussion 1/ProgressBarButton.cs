f9a89827c615c7fbd33ec8e74fece49bcbae4853
Powell <powell.payofilin2400@gmail.com>
2024-08-07 00:08:18 +0800

    refactor: created headers and comments for ProgressBarButton script

--- Assets/Scripts/Topic Discussion 1/ProgressBarButton.cs
+++ Assets/Scripts/Topic Discussion 1/ProgressBarButton.cs
@@ -7,19 +7,23 @@ public class ProgressBarButton : MonoBehaviour
 {
     public static event Action<int> ProgressBarClickEvent;
 
+    [Header("Image for Colors")]
     public Image progressBarTempColor;
     public Image progressBarFinalColor;
+    [Header("Text Holders")]
     public TextMeshProUGUI sectorTitleText;
     public TextMeshProUGUI progressCountText;
-
-    private Vector2 _startPosition;
-    private int _sectorIndex;
+    [Header("Progress Bar Button")]
     [SerializeField] private Button _progressBarButton;
+
+    // Starting position of progress bar button
+    private Vector2 _startPosition;
+    // Index to be jumped when button is pressed
+    private int _sectorIndex;
 
 
     public void Initialize(string sectorTitle, string progressCount, int index)
     {
-        
         sectorTitleText.text = sectorTitle;
         progressCountText.text = progressCount;
 
