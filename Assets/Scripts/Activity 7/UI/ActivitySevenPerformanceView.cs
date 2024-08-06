69f84d81a0a1ae63867cadfe1fe8e6731cd054f6
ndrw-ynz <andrewyanza4@gmail.com>
2024-08-06 23:36:56 +0800

    feat: add duration text in activity seven performance view

--- Assets/Scripts/Activity 7/UI/ActivitySevenPerformanceView.cs
+++ Assets/Scripts/Activity 7/UI/ActivitySevenPerformanceView.cs
@@ -12,36 +12,41 @@ public class ActivitySevenPerformanceView : MonoBehaviour
 	[Header("Center of Mass Metrics Text")]
     [SerializeField] private TextMeshProUGUI centerOfMassStatusText;
 	[SerializeField] private TextMeshProUGUI centerOfMassNumIncorrectText;
+	[SerializeField] private TextMeshProUGUI centerOfMassDurationText;
 	[Header("Momentum Impulse Force Metrics Text")]
 	[SerializeField] private TextMeshProUGUI momentumImpulseForceStatusText;
 	[SerializeField] private TextMeshProUGUI momentumImpulseForceNumIncorrectText;
+	[SerializeField] private TextMeshProUGUI momentumImpulseForceDurationText;
 	[Header("Elastic Inelastic Collision Metrics Text")]
 	[SerializeField] private TextMeshProUGUI elasticInelasticCollisionStatusText;
 	[SerializeField] private TextMeshProUGUI elasticInelasticCollisionNumIncorrectText;
+	[SerializeField] private TextMeshProUGUI elasticInelasticCollisionDurationText;
 
 	public void SetTotalTimeDisplay(float totalTime)
 	{
-		int minutes = Mathf.FloorToInt(totalTime / 60);
-		int seconds = Mathf.FloorToInt(totalTime % 60);
-		totalTimeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+		SetDurationText(totalTimeText, totalTime);
 	}
 
-	public void SetCenterOfMassMetricsDisplay(bool isAccomplished, int numIncorrectSubmission)
+	public void SetCenterOfMassMetricsDisplay(bool isAccomplished, int numIncorrectSubmission, float duration)
 	{
 		SetStatusText(centerOfMassStatusText, isAccomplished);
 		centerOfMassNumIncorrectText.text = $"{numIncorrectSubmission}";
+		SetDurationText(centerOfMassDurationText, duration);
 	}
 
-	public void SetMomentumImpulseForceMetricsDisplay(bool isAccomplished, int numIncorrectSubmission)
+	public void SetMomentumImpulseForceMetricsDisplay(bool isAccomplished, int numIncorrectSubmission, float duration)
 	{
 		SetStatusText(momentumImpulseForceStatusText, isAccomplished);
 		momentumImpulseForceNumIncorrectText.text = $"{numIncorrectSubmission}";
+		SetDurationText(momentumImpulseForceDurationText, duration);
+
 	}
 
-	public void SetElasticInelasticCollisionMetricsDisplay(bool isAccomplished, int numIncorrectSubmission)
+	public void SetElasticInelasticCollisionMetricsDisplay(bool isAccomplished, int numIncorrectSubmission, float duration)
 	{
 		SetStatusText(elasticInelasticCollisionStatusText, isAccomplished);
 		elasticInelasticCollisionNumIncorrectText.text = $"{numIncorrectSubmission}";
+		SetDurationText(elasticInelasticCollisionDurationText, duration);
 	}
 
 	private void SetStatusText(TextMeshProUGUI statusText, bool isAccomplished)
@@ -57,6 +62,13 @@ public class ActivitySevenPerformanceView : MonoBehaviour
 			statusText.color = new Color32(160, 50, 40, 255);
 		}
 	}
+
+	private void SetDurationText(TextMeshProUGUI durationText, float duration)
+	{
+		int minutes = Mathf.FloorToInt(duration / 60);
+		int seconds = Mathf.FloorToInt(duration % 60);
+		durationText.text = string.Format("{0:00}m : {1:00}s", minutes, seconds);
+	} 
 
 	public void RetryLevel()
 	{
