231686b228dab1b438b1376c8bfec5402f9ce9da
Powell <powell.payofilin2400@gmail.com>
2024-08-07 00:27:14 +0800

    refactor: made button SerializeField and created header

--- Assets/Scripts/Topic Discussion 1/UnderstoodIndicator.cs
+++ Assets/Scripts/Topic Discussion 1/UnderstoodIndicator.cs
@@ -8,18 +8,14 @@ public class UnderstoodIndicator : MonoBehaviour
 
     [Header("Object Group for Animation")]
     public CanvasGroup understoodIndicatorCanvasGroup;
-
-    [Header("Flag State To Be Used")]
+    [Header("Flag")]
     [SerializeField] private bool flag;
-
-    // Button Component of Understood Indicator
-    private Button _understoodIndicatorButton;
+    [Header("Button")]
+    [SerializeField] private Button _understoodIndicatorButton;
 
     private void OnEnable()
     {
         understoodIndicatorCanvasGroup = GetComponent<CanvasGroup>();
-
-        _understoodIndicatorButton = GetComponent<Button>();
         _understoodIndicatorButton.onClick.AddListener(() => UnderstoodIndicatorClickEvent?.Invoke(flag));
     }
 
