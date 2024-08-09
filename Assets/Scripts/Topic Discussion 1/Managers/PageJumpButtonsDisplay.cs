ebf3a4ff8322b6fc729a2866921575028ca9b556
Powell <powell.payofilin2400@gmail.com>
2024-08-09 17:03:32 +0800

    refactor: rename PageCircleButtonsDisplay script to PageJumpButtonsDisplay

--- Assets/Scripts/Topic Discussion 1/Managers/PageJumpButtonsDisplay.cs
+++ Assets/Scripts/Topic Discussion 1/Managers/PageJumpButtonsDisplay.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PageCircleButtonsDisplay : MonoBehaviour
+public class PageJumpButtonsDisplay : MonoBehaviour
 {
     [Header("Page Circle Buttons Properties")]
     [SerializeField] private PageJumpButton pageCircleButtonPrefab;
