01307b35436c4edbe8a6d93e241db5387fca9960
ndrw-ynz <andrewyanza4@gmail.com>
2024-08-31 19:43:02 +0800

    feat: add functionality for submission of force type answer in apple motion view

--- Assets/Scripts/Activity 5/UI/AppleMotion/AppleMotionView.cs
+++ Assets/Scripts/Activity 5/UI/AppleMotion/AppleMotionView.cs
@@ -5,10 +5,31 @@ public class AppleMotionView : MonoBehaviour
 {
 	public static event Action OpenViewEvent;
 	public static event Action QuitViewEvent;
+	public static event Action<ForceTypeAnswerSubmission> SubmitForceTypesAnswerEvent;
+
+	[Header("Force Type Containers")]
+	[SerializeField] private ForceTypeContainer upForceContainer;
+	[SerializeField] private ForceTypeContainer downForceContainer;
+	[SerializeField] private ForceTypeContainer leftForceContainer;
+	[SerializeField] private ForceTypeContainer rightForceContainer;
+
 
 	private void OnEnable()
 	{
 		OpenViewEvent?.Invoke();
+	}
+
+	public void OnSubmitForceTypesButtonClick()
+	{
+		ForceTypeAnswerSubmission submission = new ForceTypeAnswerSubmission(
+			forceObjectMotionType: ForceObjectMotionType.Apple_OnBranch,
+			upForceType: upForceContainer.GetCurrentForceType(),
+			downForceType: downForceContainer.GetCurrentForceType(),
+			leftForceType: leftForceContainer.GetCurrentForceType(),
+			rightForceType: rightForceContainer.GetCurrentForceType()
+			);
+
+		SubmitForceTypesAnswerEvent?.Invoke(submission);
 	}
 
 	public void OnQuitButtonClick()
