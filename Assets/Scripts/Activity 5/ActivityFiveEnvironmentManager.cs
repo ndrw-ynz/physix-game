53c3563447912128683d6b13e51bd4fbf15a6695
ndrw-ynz <andrewyanza4@gmail.com>
2024-08-31 17:15:09 +0800

    feat: add apple falling motion animation handling in scene

--- Assets/Scripts/Activity 5/ActivityFiveEnvironmentManager.cs
+++ Assets/Scripts/Activity 5/ActivityFiveEnvironmentManager.cs
@@ -5,10 +5,20 @@ public class ActivityFiveEnvironmentManager : ActivityEnvironmentManager
 	[Header("Cameras")]
 	[SerializeField] private Camera appleTreeCamera;
 
+	[Header("Motion - Apple Tree")]
+	[SerializeField] private AppleFallMotionAnimate motionApple;
+
 	public override void Start()
 	{
 		base.Start();
 
+		AppleMotionView.OpenViewEvent += () => DisplayAppleMotion(true);
 		AppleMotionView.QuitViewEvent += () => SwitchCameraToPlayerCamera(appleTreeCamera);
+		AppleMotionView.QuitViewEvent += () => DisplayAppleMotion(false);
+	}
+
+	private void DisplayAppleMotion(bool display)
+	{
+		motionApple.gameObject.SetActive(display);
 	}
 }
--- Assets/Scripts/Activity 5/UI/AppleMotion/AppleMotionView.cs
+++ Assets/Scripts/Activity 5/UI/AppleMotion/AppleMotionView.cs
@@ -3,7 +3,13 @@ using UnityEngine;
 
 public class AppleMotionView : MonoBehaviour
 {
+	public static event Action OpenViewEvent;
 	public static event Action QuitViewEvent;
+
+	private void OnEnable()
+	{
+		OpenViewEvent?.Invoke();
+	}
 
 	public void OnQuitButtonClick()
 	{
