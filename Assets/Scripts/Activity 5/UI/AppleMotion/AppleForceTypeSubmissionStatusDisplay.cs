fd3f4bb3bd16c4095a1ae08b5b05e930246c6b50
ndrw-ynz <andrewyanza4@gmail.com>
2024-09-02 18:02:08 +0800

    feat: add event for notifying update for environment state after proceed button click

--- Assets/Scripts/Activity 5/UI/AppleMotion/AppleForceTypeSubmissionStatusDisplay.cs
+++ Assets/Scripts/Activity 5/UI/AppleMotion/AppleForceTypeSubmissionStatusDisplay.cs
@@ -3,10 +3,13 @@ using System;
 public class AppleForceTypeSubmissionStatusDisplay : ForceTypeSubmissionStatusDisplay
 {
 	public static event Action ProceedEvent;
+	public static event Action<ForceObjectMotionType> UpdateAppleEnvionmentStateEvent;
+
 	protected override void OnEnable()
 	{
 		base.OnEnable();
 		proceedButton.onClick.AddListener(() => ProceedEvent?.Invoke());
+		proceedButton.onClick.AddListener(() => UpdateAppleEnvionmentStateEvent?.Invoke(displayedForceObjectMotionType));
 	}
 
 	protected override void OnDisable()
