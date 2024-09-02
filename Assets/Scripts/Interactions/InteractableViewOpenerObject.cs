14d6e395dc56616fcfd3b2bb637b8e6717d55c2b
ndrw-ynz <andrewyanza4@gmail.com>
2024-09-02 17:57:06 +0800

    refactor: remove camera component

--- Assets/Scripts/Interactions/InteractableViewOpenerObject.cs
+++ Assets/Scripts/Interactions/InteractableViewOpenerObject.cs
@@ -3,18 +3,14 @@ using UnityEngine;
 
 public class InteractableViewOpenerObject : IInteractableObject
 {
-	public static event Action<Camera> SwitchToTargetCameraEvent;
-
 	[SerializeField] private InputReader inputReader;
 	[SerializeField] private GameObject viewUI;
-	[SerializeField] private Camera targetCamera;
 	[SerializeField] private string interactionDescription;
 
 	public override void Interact()
 	{
 		inputReader.SetUI();
 		viewUI.SetActive(true);
-		SwitchToTargetCameraEvent?.Invoke(targetCamera);
 	}
 
 	public override string GetInteractionDescription()
