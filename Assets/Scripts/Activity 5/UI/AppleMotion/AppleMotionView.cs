fca510b3d8106db10bebb53b4e92fd54470f7aa7
ndrw-ynz <andrewyanza4@gmail.com>
2024-09-03 17:15:26 +0800

    feat: add component UI for force calculations, functionality for submission of answers, and configuring state of displays

--- Assets/Scripts/Activity 5/UI/AppleMotion/AppleMotionView.cs
+++ Assets/Scripts/Activity 5/UI/AppleMotion/AppleMotionView.cs
@@ -1,11 +1,29 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class AppleMotionView : MonoBehaviour
 {
 	public static event Action OpenViewEvent;
 	public static event Action QuitViewEvent;
+	public static event Action<float?> SubmitForceAnswerEvent;
 	public static event Action<ForceTypeAnswerSubmission> SubmitForceTypesAnswerEvent;
+
+	[Header("Displays")]
+	[SerializeField] private GameObject forceDiagramSelectionDisplay;
+	[SerializeField] private GameObject forceDiagramDisplay;
+	[SerializeField] private GameObject forceCalculationDisplay;
+
+	[Header("Force Given Fields")]
+	[SerializeField] private TMP_InputField givenAcceleration;
+	[SerializeField] private TMP_InputField givenMass;
+
+	[Header("Force Input Fields")]
+	[SerializeField] private TMP_InputField forceMultiplicand;
+	[SerializeField] private TMP_InputField forceMultiplier;
+
+	[Header("Force Result Field")]
+	[SerializeField] private TMP_InputField forceResultField;
 
 	[Header("Force Type Containers")]
 	[SerializeField] private ForceTypeContainer upForceContainer;
@@ -19,10 +37,58 @@ public class AppleMotionView : MonoBehaviour
 		OpenViewEvent?.Invoke();
 	}
 
+	public void SetupForceCalculationDisplay(ForceData data)
+	{
+		ClearAllFields();
+
+		switch(ActivityFiveManager.difficultyConfiguration)
+		{
+			case Difficulty.Easy:
+				givenAcceleration.text = $"{data.acceleration} m/s^2";
+				givenMass.text = $"{data.mass} kg";
+				break;
+			case Difficulty.Medium:
+			case Difficulty.Hard:
+				givenAcceleration.text = $"{data.acceleration * 0.001} km/s^2";
+				givenMass.text = $"{data.mass * 1000} g";
+				break;
+		}
+	}
+
+	public void ClearAllFields()
+	{
+		forceMultiplicand.text = "0";
+		forceMultiplier.text = "0";
+	}
+
+	public void ResetForceDiagram()
+	{
+		upForceContainer.ClearContainer();
+		downForceContainer.ClearContainer();
+		leftForceContainer.ClearContainer();
+		rightForceContainer.ClearContainer();
+	}
+
+	public void SetForceCalculationDisplayState(bool isActive)
+	{
+		forceCalculationDisplay.gameObject.SetActive(isActive);
+	}
+
+	public void SetForceDiagramDisplayState(bool isActive)
+	{
+		forceDiagramSelectionDisplay.gameObject.SetActive(isActive);
+		forceDiagramDisplay.gameObject.SetActive(isActive);
+	}
+
+	public void OnSubmitForceButtonClick()
+	{
+		bool canParse = float.TryParse(forceResultField.text, out float result);
+		SubmitForceAnswerEvent?.Invoke(canParse ? result : null);
+	}
+
 	public void OnSubmitForceTypesButtonClick()
 	{
 		ForceTypeAnswerSubmission submission = new ForceTypeAnswerSubmission(
-			forceObjectMotionType: ForceObjectMotionType.Apple_OnBranch,
 			upForceType: upForceContainer.GetCurrentForceType(),
 			downForceType: downForceContainer.GetCurrentForceType(),
 			leftForceType: leftForceContainer.GetCurrentForceType(),
