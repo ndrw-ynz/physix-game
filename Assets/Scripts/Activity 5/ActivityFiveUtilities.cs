e27ef196c1dce681ed50958aa593027b6b08c953
ndrw-ynz <andrewyanza4@gmail.com>
2024-09-04 23:36:11 +0800

    feat: add handling of rock force diagram validation in ValidateForceTypeSubmission utility function

--- Assets/Scripts/Activity 5/ActivityFiveUtilities.cs
+++ Assets/Scripts/Activity 5/ActivityFiveUtilities.cs
@@ -37,6 +37,30 @@ public static class ActivityFiveUtilities
 				results.isLeftForceTypeCorrect = submission.leftForceType == null;
 				results.isRightForceTypeCorrect = submission.rightForceType == null;
 				break;
+			case ForceObjectMotionType.Rock_Stationary:
+				results.isUpForceTypeCorrect = submission.upForceType == ForceType.NormalForce;
+				results.isDownForceTypeCorrect = submission.downForceType == ForceType.GravitationalForce;
+				results.isLeftForceTypeCorrect = submission.leftForceType == null;
+				results.isRightForceTypeCorrect = submission.rightForceType == null;
+				break;
+			case ForceObjectMotionType.Rock_RollingRight:
+				results.isUpForceTypeCorrect = submission.upForceType == ForceType.NormalForce;
+				results.isDownForceTypeCorrect = submission.downForceType == ForceType.GravitationalForce;
+				results.isLeftForceTypeCorrect = submission.leftForceType == ForceType.FrictionalForce;
+				results.isRightForceTypeCorrect = submission.rightForceType == ForceType.AppliedForce;
+				break;
+			case ForceObjectMotionType.Rock_Bouncing:
+				results.isUpForceTypeCorrect = submission.upForceType == ForceType.NormalForce;
+				results.isDownForceTypeCorrect = submission.downForceType == ForceType.GravitationalForce;
+				results.isLeftForceTypeCorrect = submission.leftForceType == ForceType.SpringForce;
+				results.isRightForceTypeCorrect = submission.rightForceType == null;
+				break;
+			case ForceObjectMotionType.Rock_Flying:
+				results.isUpForceTypeCorrect = submission.upForceType == null;
+				results.isDownForceTypeCorrect = submission.downForceType == ForceType.GravitationalForce;
+				results.isLeftForceTypeCorrect = submission.leftForceType == null;
+				results.isRightForceTypeCorrect = submission.rightForceType == ForceType.DragForce;
+				break;
 		}
 		return results;
 	}
