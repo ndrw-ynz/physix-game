b44640463145467ba2a6772d8df33a948db0951d
ndrw-ynz <andrewyanza4@gmail.com>
2024-09-04 23:38:36 +0800

    feat: add gameobject components of isolated rock area,  associated rock views and submission status displays, and handling of environment state machine for managing rock area states

--- Assets/Scripts/Activity 5/ActivityFiveEnvironmentManager.cs
+++ Assets/Scripts/Activity 5/ActivityFiveEnvironmentManager.cs
@@ -5,15 +5,23 @@ public class ActivityFiveEnvironmentManager : ActivityEnvironmentManager
 {
 	[Header("Views")]
 	[SerializeField] private ForceMotionView appleMotionView;
+	[SerializeField] private ForceMotionView rockMotionView;
 
 	[Header("Submission Status Displays")]
 	[Header("Apple Force Submission Status Displays")]
 	[SerializeField] private ForceSubmissionStatusDisplay appleForceSubmissionStatusDisplay;
 	[SerializeField] private ForceDiagramSubmissionStatusDisplay appleForceDiagramSubmissionStatusDisplay;
+	[Header("Rock Force Submission Status Displays")]
+	[SerializeField] private ForceSubmissionStatusDisplay rockForceSubmissionStatusDisplay;
+	[SerializeField] private ForceDiagramSubmissionStatusDisplay rockForceDiagramSubmissionStatusDisplay;
 
 	[Header("Environment Cameras")]
-	[SerializeField] private Camera appleCamera;
+	[SerializeField] private Camera appleOnBranchCamera;
 	[SerializeField] private Camera appleTreeCamera;
+	[SerializeField] private Camera stationaryRockCamera;
+	[SerializeField] private Camera rollingRockCamera;
+	[SerializeField] private Camera bouncingRockCamera;
+	[SerializeField] private Camera flyingRockCamera;
 
 	[Header("Apple Tree Area Game Objects")]
 	[SerializeField] private GameObject appleOnBranch;
@@ -21,8 +29,18 @@ public class ActivityFiveEnvironmentManager : ActivityEnvironmentManager
 	[SerializeField] private GameObject appleTreeAreaIndicatorEffect;
 	[SerializeField] private InteractableViewOpenerObject interactableApples;
 
+	[Header("Rock Area Game Objects")]
+	[SerializeField] private GameObject stationaryRock;
+	[SerializeField] private RollingRockMotionAnimate rollingRock;
+	[SerializeField] private BouncingRockMotionAnimate bouncingRock;
+	[SerializeField] private FlyingRockAnimate flyingRock;
+	[SerializeField] private GameObject rockAreaIndicatorEffect;
+	[SerializeField] private InteractableViewOpenerObject interactableRock;
+
 	private AppleMotionEnvironmentStateMachine appleMotionEnvironmentStateMachine;
 	private Queue<AppleMotionEnvironmentState> appleMotionEnvironmentStateQueue;
+	private RockMotionEnvironmentStateMachine rockMotionEnvironmentStateMachine;
+	private Queue<RockMotionEnvironmentState> rockMotionEnvironmentStateQueue;
 
 	public void Start()
 	{
@@ -30,13 +48,21 @@ public class ActivityFiveEnvironmentManager : ActivityEnvironmentManager
 		appleMotionView.QuitViewEvent += () => appleMotionEnvironmentStateMachine.TransitionToState(AppleMotionEnvironmentState.None);
 		appleForceSubmissionStatusDisplay.ProceedEvent += DequeueAppleEnvironmentStateQueue;
 		appleForceDiagramSubmissionStatusDisplay.ProceedEvent += DequeueAppleEnvironmentStateQueue;
-		
+
+		rockMotionView.OpenViewEvent += UpdateRockEnvironmentStateMachine;
+		rockMotionView.QuitViewEvent += () => rockMotionEnvironmentStateMachine.TransitionToState(RockMotionEnvironmentState.None);
+		rockForceSubmissionStatusDisplay.ProceedEvent += DequeueRockEnvironmentStateQueue;
+		rockForceDiagramSubmissionStatusDisplay.ProceedEvent += DequeueRockEnvironmentStateQueue;
+
 		// Initialize environment state queues
 		InitializeEnvironmentStateQueues();
 
 		// Initialize values for apple tree environment state machine
 		appleMotionEnvironmentStateMachine = new AppleMotionEnvironmentStateMachine(this);
 		appleMotionEnvironmentStateMachine.Initialize(AppleMotionEnvironmentState.None);
+		// Initialize values for rock environment state machine
+		rockMotionEnvironmentStateMachine = new RockMotionEnvironmentStateMachine(this);
+		rockMotionEnvironmentStateMachine.Initialize(RockMotionEnvironmentState.None);
 	}
 
 	private void InitializeEnvironmentStateQueues()
@@ -47,22 +73,38 @@ public class ActivityFiveEnvironmentManager : ActivityEnvironmentManager
 		appleMotionEnvironmentStateQueue.Enqueue(AppleMotionEnvironmentState.Falling);
 		appleMotionEnvironmentStateQueue.Enqueue(AppleMotionEnvironmentState.Falling);
 
+		rockMotionEnvironmentStateQueue = new Queue<RockMotionEnvironmentState>();
+		rockMotionEnvironmentStateQueue.Enqueue(RockMotionEnvironmentState.Stationary);
+		rockMotionEnvironmentStateQueue.Enqueue(RockMotionEnvironmentState.Rolling);
+		rockMotionEnvironmentStateQueue.Enqueue(RockMotionEnvironmentState.Rolling);
+		rockMotionEnvironmentStateQueue.Enqueue(RockMotionEnvironmentState.Bouncing);
+		rockMotionEnvironmentStateQueue.Enqueue(RockMotionEnvironmentState.Flying);
+		rockMotionEnvironmentStateQueue.Enqueue(RockMotionEnvironmentState.Flying);
+
 		// Enqueue addition environment states based on difficulty configuration
 		switch (ActivityFiveManager.difficultyConfiguration)
 		{
 			case Difficulty.Medium:
 				appleMotionEnvironmentStateQueue.Enqueue(AppleMotionEnvironmentState.Falling);
+
+				rockMotionEnvironmentStateQueue.Enqueue(RockMotionEnvironmentState.Rolling);
+				rockMotionEnvironmentStateQueue.Enqueue(RockMotionEnvironmentState.Flying);
 				break;
 			case Difficulty.Hard:
 				appleMotionEnvironmentStateQueue.Enqueue(AppleMotionEnvironmentState.Falling);
 				appleMotionEnvironmentStateQueue.Enqueue(AppleMotionEnvironmentState.Falling);
+
+				rockMotionEnvironmentStateQueue.Enqueue(RockMotionEnvironmentState.Rolling);
+				rockMotionEnvironmentStateQueue.Enqueue(RockMotionEnvironmentState.Flying);
+				rockMotionEnvironmentStateQueue.Enqueue(RockMotionEnvironmentState.Rolling);
+				rockMotionEnvironmentStateQueue.Enqueue(RockMotionEnvironmentState.Flying);
 				break;
 		}
 	}
 
 	public void SetAppleOnBranchState(bool isActive)
 	{
-		appleCamera.gameObject.SetActive(isActive);
+		appleOnBranchCamera.gameObject.SetActive(isActive);
 		appleOnBranch.gameObject.SetActive(isActive);
 	}
 
@@ -91,6 +133,52 @@ public class ActivityFiveEnvironmentManager : ActivityEnvironmentManager
 		else
 		{
 			appleMotionEnvironmentStateMachine.TransitionToState(appleMotionEnvironmentStateQueue.Peek());
+		}
+	}
+
+	public void SetRockStationaryState(bool isActive)
+	{
+		stationaryRockCamera.gameObject.SetActive(isActive);
+		stationaryRock.gameObject.SetActive(isActive);
+	}
+
+	public void SetRockRollingState(bool isActive)
+	{
+		rollingRockCamera.gameObject.SetActive(isActive);
+		rollingRock.gameObject.SetActive(isActive);
+	}
+
+	public void SetRockBouncingState(bool isActive)
+	{
+		bouncingRockCamera.gameObject.SetActive(isActive);
+		bouncingRock.gameObject.SetActive(isActive);
+	}
+
+	public void SetRockFlyingState(bool isActive)
+	{
+		flyingRockCamera.gameObject.SetActive(isActive);
+		flyingRock.gameObject.SetActive(isActive);
+	}
+
+	private void DequeueRockEnvironmentStateQueue()
+	{
+		rockMotionEnvironmentStateQueue.Dequeue();
+		UpdateRockEnvironmentStateMachine();
+	}
+
+	private void UpdateRockEnvironmentStateMachine()
+	{
+		if (rockMotionEnvironmentStateQueue.Count == 0)
+		{
+			// Deactivate area effect and interactable rock
+			rockAreaIndicatorEffect.gameObject.SetActive(false);
+			interactableRock.SetInteractable(false);
+
+			rockMotionEnvironmentStateMachine.TransitionToState(RockMotionEnvironmentState.None);
+		}
+		else
+		{
+			rockMotionEnvironmentStateMachine.TransitionToState(rockMotionEnvironmentStateQueue.Peek());
 		}
 	}
 }
