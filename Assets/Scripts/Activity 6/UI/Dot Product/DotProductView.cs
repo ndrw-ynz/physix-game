2e7c1d4ec1ecc617c8c8f24a81d7e89479a346b8
ndrw-ynz <andrewyanza4@gmail.com>
2024-09-10 12:14:44 +0800

    feat: add given fields and equation displays related to dot product, and functionality for setting up view

--- Assets/Scripts/Activity 6/UI/Dot Product/DotProductView.cs
+++ Assets/Scripts/Activity 6/UI/Dot Product/DotProductView.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class DotProductView : MonoBehaviour
@@ -6,9 +7,34 @@ public class DotProductView : MonoBehaviour
 	public event Action OpenViewEvent;
 	public event Action QuitViewEvent;
 
+	[Header("Vector Given Fields")]
+	[SerializeField] private TMP_InputField satelliteDishVector;
+	[SerializeField] private TMP_InputField targetObjectVector;
+
+	[Header("Dot Product Equation Displays")]
+	[SerializeField] private ProductEquationDisplay xCoordScalarProductDisplay;
+	[SerializeField] private ProductEquationDisplay yCoordScalarProductDisplay;
+	[SerializeField] private ProductEquationDisplay zCoordScalarProductDisplay;
+	[SerializeField] private SumEquationDisplay dotProductDisplay;
+
 	private void OnEnable()
 	{
 		OpenViewEvent?.Invoke();
+	}
+
+	public void SetupDotProductView(DotProductData data)
+	{
+		ClearAllFields();
+		satelliteDishVector.text = $"({data.satelliteDishVector.x}, {data.satelliteDishVector.y}, {data.satelliteDishVector.z})";
+		targetObjectVector.text = $"({data.targetObjectVector.x}, {data.targetObjectVector.y}, {data.targetObjectVector.z})";
+	}
+
+	private void ClearAllFields()
+	{
+		xCoordScalarProductDisplay.ResetState();
+		yCoordScalarProductDisplay.ResetState();
+		zCoordScalarProductDisplay.ResetState();
+		dotProductDisplay.ResetState();
 	}
 
 	public void OnQuitButtonClick()
