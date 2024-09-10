0eece737ad34a3e4d3c263ef9eaca1ad1d178034
ndrw-ynz <andrewyanza4@gmail.com>
2024-09-10 12:49:48 +0800

    feat: add functionality for submitting dot product answer with SubmitAnswerEvent

--- Assets/Scripts/Activity 6/UI/Dot Product/DotProductView.cs
+++ Assets/Scripts/Activity 6/UI/Dot Product/DotProductView.cs
@@ -2,10 +2,33 @@ using System;
 using TMPro;
 using UnityEngine;
 
+public class DotProductAnswerSubmission
+{
+	public float? xCoordScalarProduct { get; private set; }
+	public float? yCoordScalarProduct { get; private set; }
+	public float? zCoordScalarProduct { get; private set; }
+
+	public float? dotProduct { get; private set; }
+
+	public DotProductAnswerSubmission(
+		float? xCoordScalarProduct,
+		float? yCoordScalarProduct,
+		float? zCoordScalarProduct,
+		float? dotProduct
+		)
+	{
+		this.xCoordScalarProduct = xCoordScalarProduct;
+		this.yCoordScalarProduct = yCoordScalarProduct;
+		this.zCoordScalarProduct = zCoordScalarProduct;
+		this.dotProduct = dotProduct;
+	}
+}
+
 public class DotProductView : MonoBehaviour
 {
 	public event Action OpenViewEvent;
 	public event Action QuitViewEvent;
+	public event Action<DotProductAnswerSubmission> SubmitAnswerEvent;
 
 	[Header("Vector Given Fields")]
 	[SerializeField] private TMP_InputField satelliteDishVector;
@@ -35,6 +58,18 @@ public class DotProductView : MonoBehaviour
 		yCoordScalarProductDisplay.ResetState();
 		zCoordScalarProductDisplay.ResetState();
 		dotProductDisplay.ResetState();
+	}
+
+	public void OnSubmitButtonClick()
+	{
+		DotProductAnswerSubmission submission = new DotProductAnswerSubmission(
+			xCoordScalarProduct: xCoordScalarProductDisplay.productValue,
+			yCoordScalarProduct: yCoordScalarProductDisplay.productValue,
+			zCoordScalarProduct: zCoordScalarProductDisplay.productValue,
+			dotProduct: dotProductDisplay.sumValue
+			);
+
+		SubmitAnswerEvent?.Invoke(submission);
 	}
 
 	public void OnQuitButtonClick()
