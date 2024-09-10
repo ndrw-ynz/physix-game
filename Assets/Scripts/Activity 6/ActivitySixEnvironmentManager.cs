4f938ab6008e1425bf569777e518c31414a8dbf2
ndrw-ynz <andrewyanza4@gmail.com>
2024-09-10 15:27:17 +0800

    feat: add gameobject components of main satellite area and configuring of environment state on clear of area from MainSatelliteAreaClearEvent from main manager

--- Assets/Scripts/Activity 6/ActivitySixEnvironmentManager.cs
+++ Assets/Scripts/Activity 6/ActivitySixEnvironmentManager.cs
@@ -1,8 +1,10 @@
-using System.Collections;
 using UnityEngine;
 
 public class ActivitySixEnvironmentManager : ActivityEnvironmentManager
 {
+	[Header("Activity Manager")]
+	[SerializeField] private ActivitySixManager activitySixManager;
+
     [Header("Views")]
     [SerializeField] private DotProductView dotProductView;
 
@@ -10,6 +12,10 @@ public class ActivitySixEnvironmentManager : ActivityEnvironmentManager
 	[SerializeField] private Camera satelliteEnvironmentCamera;
 	[SerializeField] private Camera mainSatelliteCamera;
 	[SerializeField] private Camera targetObjectCamera;
+
+	[Header("Main Satellite Area Game Objects")]
+	[SerializeField] private InteractableViewOpenerObject mainSatelliteControlPanel;
+	[SerializeField] private GameObject mainSatelliteAreaIndicatorEffect;
 
 	private void Start()
 	{
@@ -21,6 +27,7 @@ public class ActivitySixEnvironmentManager : ActivityEnvironmentManager
 		// Satellite environment area related events
 		dotProductView.OpenViewEvent += () => SetSatelliteEnvironmentState(true);
 		dotProductView.QuitViewEvent += () => SetSatelliteEnvironmentState(false);
+		activitySixManager.MainSatelliteAreaClearEvent += ClearSatelliteEnvironmentState;
 	}
 
 	private void SetSatelliteEnvironmentState(bool isActive)
@@ -29,5 +36,12 @@ public class ActivitySixEnvironmentManager : ActivityEnvironmentManager
 		satelliteEnvironmentCamera.gameObject.SetActive(isActive);
 		mainSatelliteCamera.gameObject.SetActive(isActive);
 		targetObjectCamera.gameObject.SetActive(isActive);
+	}
+
+	private void ClearSatelliteEnvironmentState()
+	{
+		SetSatelliteEnvironmentState(false);
+		mainSatelliteControlPanel.SetInteractable(false);
+		mainSatelliteAreaIndicatorEffect.gameObject.SetActive(false);
 	}
 }
