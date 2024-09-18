83a16e45c9d2108f68c47980758e090cd8584a97
ndrw-ynz <andrewyanza4@gmail.com>
2024-09-18 16:11:09 +0800

    feat: add button switch functionality for pause menu ui

--- Assets/Scripts/Common Activity Scripts/UI/ActivityPauseMenuUI.cs
+++ Assets/Scripts/Common Activity Scripts/UI/ActivityPauseMenuUI.cs
@@ -20,17 +20,61 @@ public class ActivityPauseMenuUI : MonoBehaviour
 	[SerializeField] private Button topicDiscussionButton;
 	[SerializeField] private Button quitActivityButton;
 
+	private List<Button> interactableButtons;
+	private int selectedButtonIndex;
+
 	private void Start()
 	{
 		inputReader.ResumeGameplayEvent += ResumeActivity;
+		inputReader.PauseMenuNavigationEvent += HandleButtonNavigationChange;
 
+		InitializeInteractableButtons();
+	}
+
+	private void InitializeInteractableButtons()
+	{
+		interactableButtons = new List<Button>
+		{
+			resumeActivityButton,
+			topicDiscussionButton,
+			quitActivityButton
+		};
 		resumeActivityButton.onClick.AddListener(() => ResumeActivity());
 		topicDiscussionButton.onClick.AddListener(() => GoToTopicDiscussion());
 		quitActivityButton.onClick.AddListener(() => QuitActivity());
 	}
 
+	private void HandleButtonNavigationChange(Vector2 obj)
+	{
+		selectedButtonIndex = (selectedButtonIndex - (int) obj.y + interactableButtons.Count) % interactableButtons.Count;
+		UpdateSelectedButtonState(selectedButtonIndex);
+	}
+
+	private void UpdateSelectedButtonState(int selectedIndex = 0)
+	{
+		selectedButtonIndex = selectedIndex;
+
+		for (int i = 0; i < interactableButtons.Count; i++)
+		{
+			TextMeshProUGUI currentButtonText = interactableButtons[i].GetComponent<TextMeshProUGUI>();
+			if (i == selectedButtonIndex)
+			{
+				currentButtonText.fontSize = 86;
+				currentButtonText.fontStyle = FontStyles.Bold;
+			}
+			else
+			{
+				currentButtonText.fontSize = 56;
+				currentButtonText.fontStyle = FontStyles.Normal;
+			}
+		}
+	}
+
 	public void UpdateContent(string headerText, List<string> tasksText, List<string> objectivesText)
 	{
+		if (interactableButtons == null) InitializeInteractableButtons();
+		UpdateSelectedButtonState();
+
 		this.headerText.text = headerText;
 
 		this.tasksText.text = "";
