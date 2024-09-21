06d294dae29bda5cadd43112dac63ffd0fceb953
ndrw-ynz <andrewyanza4@gmail.com>
2024-09-21 18:32:17 +0800

    refactor: implement ActivityPerformanceView base class

--- Assets/Scripts/Activity 8/UI/ActivityEightPerformanceView.cs
+++ Assets/Scripts/Activity 8/UI/ActivityEightPerformanceView.cs
@@ -2,14 +2,8 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ActivityEightPerformanceView : MonoBehaviour
+public class ActivityEightPerformanceView : ActivityPerformanceView
 {
-	[Header("Input Reader")]
-	[SerializeField] InputReader inputReader;
-
-	[Header("Total Time Text")]
-	[SerializeField] private TextMeshProUGUI totalTimeText;
-
 	[Header("Moment of Inertia Metrics Text")]
 	[SerializeField] private TextMeshProUGUI momentOfInertiaStatusText;
 	[SerializeField] private TextMeshProUGUI momentOfInertiaNumIncorrectText;
@@ -23,10 +17,6 @@ public class ActivityEightPerformanceView : MonoBehaviour
 	[SerializeField] private TextMeshProUGUI equilibriumNumIncorrectText;
 	[SerializeField] private TextMeshProUGUI equilibriumDurationText;
 
-	public void SetTotalTimeDisplay(float totalTime)
-	{
-		SetDurationText(totalTimeText, totalTime);
-	}
 	public void SetMomentOfInertiaMetricsDisplay(bool isAccomplished, int numIncorrectSubmission, float duration)
 	{
 		SetStatusText(momentOfInertiaStatusText, isAccomplished);
@@ -49,44 +39,23 @@ public class ActivityEightPerformanceView : MonoBehaviour
 		SetDurationText(equilibriumDurationText, duration);
 	}
 
-	private void SetStatusText(TextMeshProUGUI statusText, bool isAccomplished)
-	{
-		if (isAccomplished)
-		{
-			statusText.text = "Accomplished";
-			statusText.color = new Color32(70, 160, 40, 255);
-		}
-		else
-		{
-			statusText.text = "Not Accomplished";
-			statusText.color = new Color32(160, 50, 40, 255);
-		}
-	}
-
-	private void SetDurationText(TextMeshProUGUI durationText, float duration)
-	{
-		int minutes = Mathf.FloorToInt(duration / 60);
-		int seconds = Mathf.FloorToInt(duration % 60);
-		durationText.text = string.Format("{0:00}m : {1:00}s", minutes, seconds);
-	}
-
-	public void RetryLevel()
+	public override void RetryLevel()
 	{
 		/*inputReader.SetGameplay();
 		SceneManager.LoadScene(SceneManager.GetActiveScene().name);*/
 	}
 
-	public void ReturnMainMenu()
+	public override void ReturnMainMenu()
 	{
 		SceneManager.LoadScene("Main Menu");
 	}
 
-	public void ProceedToNextLevel()
+	public override void ProceedToNextLevel()
 	{
 		// IN THE FUTURE, ADD SCENE FOR TOPIC DISCUSSION
 	}
 
-	public void GoToSelectionScreen()
+	public override void GoToSelectionScreen()
 	{
 		// IN THE FUTURE, ADD SCENE FOR SELECTION
 	}
