e8b820bbec950fe9251e869be110a5aec89479ff
ndrw-ynz <andrewyanza4@gmail.com>
2024-09-21 18:47:27 +0800

    refactor: implement ActivityPerformanceView base class for better code structure

--- Assets/Scripts/Activity 9/UI/ActivityNinePerformanceView.cs
+++ Assets/Scripts/Activity 9/UI/ActivityNinePerformanceView.cs
@@ -2,23 +2,13 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ActivityNinePerformanceView : MonoBehaviour
+public class ActivityNinePerformanceView : ActivityPerformanceView
 {
-	[Header("Input Reader")]
-	[SerializeField] InputReader inputReader;
-
-	[Header("Total Time Text")]
-	[SerializeField] private TextMeshProUGUI totalTimeText;
-
 	[Header("Gravity Metrics Text")]
 	[SerializeField] private TextMeshProUGUI gravityStatusText;
 	[SerializeField] private TextMeshProUGUI gravityNumIncorrectText;
 	[SerializeField] private TextMeshProUGUI gravityDurationText;
 
-	public void SetTotalTimeDisplay(float totalTime)
-	{
-		SetDurationText(totalTimeText, totalTime);
-	}
 	public void SetGravityMetricsDisplay(bool isAccomplished, int numIncorrectSubmission, float duration)
 	{
 		SetStatusText(gravityStatusText, isAccomplished);
@@ -26,44 +16,23 @@ public class ActivityNinePerformanceView : MonoBehaviour
 		SetDurationText(gravityDurationText, duration);
 	}
 
-	private void SetStatusText(TextMeshProUGUI statusText, bool isAccomplished)
-	{
-		if (isAccomplished)
-		{
-			statusText.text = "Accomplished";
-			statusText.color = new Color32(70, 160, 40, 255);
-		}
-		else
-		{
-			statusText.text = "Not Accomplished";
-			statusText.color = new Color32(160, 50, 40, 255);
-		}
-	}
-
-	private void SetDurationText(TextMeshProUGUI durationText, float duration)
-	{
-		int minutes = Mathf.FloorToInt(duration / 60);
-		int seconds = Mathf.FloorToInt(duration % 60);
-		durationText.text = string.Format("{0:00}m : {1:00}s", minutes, seconds);
-	}
-
-	public void RetryLevel()
+	public override void RetryLevel()
 	{
 		/*inputReader.SetGameplay();
 		SceneManager.LoadScene(SceneManager.GetActiveScene().name);*/
 	}
 
-	public void ReturnMainMenu()
+	public override void ReturnMainMenu()
 	{
 		SceneManager.LoadScene("Main Menu");
 	}
 
-	public void ProceedToNextLevel()
+	public override void ProceedToNextLevel()
 	{
 		// IN THE FUTURE, ADD SCENE FOR TOPIC DISCUSSION
 	}
 
-	public void GoToSelectionScreen()
+	public override void GoToSelectionScreen()
 	{
 		// IN THE FUTURE, ADD SCENE FOR SELECTION
 	}
