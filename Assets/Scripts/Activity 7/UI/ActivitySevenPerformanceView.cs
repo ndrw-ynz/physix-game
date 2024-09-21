f6ead6f40cf89894f577f510cb7b43e2d2285851
ndrw-ynz <andrewyanza4@gmail.com>
2024-09-21 18:07:11 +0800

    refactor: implement ActivityPerofrmanceView base class for better code structure

--- Assets/Scripts/Activity 7/UI/ActivitySevenPerformanceView.cs
+++ Assets/Scripts/Activity 7/UI/ActivitySevenPerformanceView.cs
@@ -2,13 +2,8 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ActivitySevenPerformanceView : MonoBehaviour
+public class ActivitySevenPerformanceView : ActivityPerformanceView
 {
-	[Header("Input Reader")]
-	[SerializeField] InputReader inputReader;
-
-	[Header("Total Time Text")]
-	[SerializeField] private TextMeshProUGUI totalTimeText;
 	[Header("Center of Mass Metrics Text")]
     [SerializeField] private TextMeshProUGUI centerOfMassStatusText;
 	[SerializeField] private TextMeshProUGUI centerOfMassNumIncorrectText;
@@ -21,11 +16,6 @@ public class ActivitySevenPerformanceView : MonoBehaviour
 	[SerializeField] private TextMeshProUGUI elasticInelasticCollisionStatusText;
 	[SerializeField] private TextMeshProUGUI elasticInelasticCollisionNumIncorrectText;
 	[SerializeField] private TextMeshProUGUI elasticInelasticCollisionDurationText;
-
-	public void SetTotalTimeDisplay(float totalTime)
-	{
-		SetDurationText(totalTimeText, totalTime);
-	}
 
 	public void SetCenterOfMassMetricsDisplay(bool isAccomplished, int numIncorrectSubmission, float duration)
 	{
@@ -49,44 +39,23 @@ public class ActivitySevenPerformanceView : MonoBehaviour
 		SetDurationText(elasticInelasticCollisionDurationText, duration);
 	}
 
-	private void SetStatusText(TextMeshProUGUI statusText, bool isAccomplished)
-	{
-		if (isAccomplished)
-		{
-			statusText.text = "Accomplished";
-			statusText.color = new Color32(70, 160, 40, 255);
-		}
-		else
-		{
-			statusText.text = "Not Accomplished";
-			statusText.color = new Color32(160, 50, 40, 255);
-		}
-	}
-
-	private void SetDurationText(TextMeshProUGUI durationText, float duration)
-	{
-		int minutes = Mathf.FloorToInt(duration / 60);
-		int seconds = Mathf.FloorToInt(duration % 60);
-		durationText.text = string.Format("{0:00}m : {1:00}s", minutes, seconds);
-	} 
-
-	public void RetryLevel()
+	public override void RetryLevel()
 	{
 		/*inputReader.SetGameplay();
 		SceneManager.LoadScene(SceneManager.GetActiveScene().name);*/
 	}
 
-	public void ReturnMainMenu()
+	public override void ReturnMainMenu()
 	{
 		SceneManager.LoadScene("Main Menu");
 	}
 
-	public void GoToTopicDiscussion()
+	public override void ProceedToNextLevel()
 	{
 		// IN THE FUTURE, ADD SCENE FOR TOPIC DISCUSSION
 	}
 
-	public void GoToSelectionScreen()
+	public override void GoToSelectionScreen()
 	{
 		// IN THE FUTURE, ADD SCENE FOR SELECTION
 	}
