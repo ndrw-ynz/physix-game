0e6a62af721cb71ce77a1ea4c248d8c7bc402a30
ndrw-ynz <andrewyanza4@gmail.com>
2024-10-11 22:45:02 +0800

    feat: add components for scientific notation room and functionality for changing its environment state based on open and quit events on associated view

--- Assets/Scripts/Activity 1/ActivityOneEnvironmentManager.cs
+++ Assets/Scripts/Activity 1/ActivityOneEnvironmentManager.cs
@@ -4,19 +4,30 @@ public class ActivityOneEnvironmentManager : ActivityEnvironmentManager
 {
 	[Header("Views")]
 	[SerializeField] private ContainerPickerView containerPickerView;
+	[SerializeField] private ScientificNotationView scientificNotationView;
 
 	[Header("Environment Cameras")]
 	[SerializeField] private Camera containerEnvironmentCamera;
-	
+	[SerializeField] private Camera SNRoomEnvironmentCamera;
+
 	private void Start()
 	{
 		containerPickerView.OpenViewEvent += () => SetContainerAreaEnvironmentState(true);
 		containerPickerView.QuitViewEvent += () => SetContainerAreaEnvironmentState(false);
+
+		scientificNotationView.OpenViewEvent += () => SetSNRoomAreaEnvironmentState(true);
+		scientificNotationView.QuitViewEvent += () => SetSNRoomAreaEnvironmentState(false);
 	}
 
 	private void SetContainerAreaEnvironmentState(bool isActive)
 	{
 		SetPlayerActivityState(!isActive);
 		containerEnvironmentCamera.gameObject.SetActive(isActive);
+	}
+
+	private void SetSNRoomAreaEnvironmentState(bool isActive)
+	{
+		SetPlayerActivityState(!isActive);
+		SNRoomEnvironmentCamera.gameObject.SetActive(isActive);
 	}
 }
