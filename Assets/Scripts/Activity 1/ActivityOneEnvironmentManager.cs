0d6a5296498a0b13d667a8cf4ef6df0ac99636d3
ndrw-ynz <andrewyanza4@gmail.com>
2024-10-12 16:42:00 +0800

    feat: add components of scientific notation terminal area, and functionality for resetting its state on clear and correct answer in activity

--- Assets/Scripts/Activity 1/ActivityOneEnvironmentManager.cs
+++ Assets/Scripts/Activity 1/ActivityOneEnvironmentManager.cs
@@ -1,7 +1,14 @@
+using System.Xml.Linq;
 using UnityEngine;
 
 public class ActivityOneEnvironmentManager : ActivityEnvironmentManager
 {
+	[Header("Activity Manager")]
+	[SerializeField] private ActivityOneManager activityOneManager;
+
+	[Header("Handlers")]
+	[SerializeField] private ContainerSelectionHandler containerSelectionHandler;
+
 	[Header("Views")]
 	[SerializeField] private ContainerPickerView containerPickerView;
 	[SerializeField] private ScientificNotationView scientificNotationView;
@@ -10,6 +17,13 @@ public class ActivityOneEnvironmentManager : ActivityEnvironmentManager
 	[SerializeField] private Camera containerEnvironmentCamera;
 	[SerializeField] private Camera SNRoomEnvironmentCamera;
 
+	[Header("Scientific Notation Terminal Area Game Objects")]
+	[SerializeField] private InteractableViewOpenerObject containerPickerControlPanel;
+	[SerializeField] private InteractableViewOpenerObject scientificNotationControlPanel;
+	[SerializeField] private GameObject displayedContainerObject;
+	[SerializeField] private GameObject scientificNotationTransitionDoor;
+	[SerializeField] private GameObject scientificNotationTransitionBarrier;
+
 	private void Start()
 	{
 		containerPickerView.OpenViewEvent += () => SetContainerAreaEnvironmentState(true);
@@ -17,8 +31,14 @@ public class ActivityOneEnvironmentManager : ActivityEnvironmentManager
 
 		scientificNotationView.OpenViewEvent += () => SetSNRoomAreaEnvironmentState(true);
 		scientificNotationView.QuitViewEvent += () => SetSNRoomAreaEnvironmentState(false);
+
+		containerSelectionHandler.UpdateSelectedContainerEvent += (boxContainer) => displayedContainerObject.SetActive(boxContainer != null);
+
+		activityOneManager.SNRoomClearEvent += ClearSNRoomEnvironmentState;
+		activityOneManager.SNCorrectAnswerEvent += ResetSNRoomAreaEnvironmentState;
 	}
 
+	#region Scientific Notation Terminal Room
 	private void SetContainerAreaEnvironmentState(bool isActive)
 	{
 		SetPlayerActivityState(!isActive);
@@ -30,4 +50,24 @@ public class ActivityOneEnvironmentManager : ActivityEnvironmentManager
 		SetPlayerActivityState(!isActive);
 		SNRoomEnvironmentCamera.gameObject.SetActive(isActive);
 	}
+
+	private void ResetSNRoomAreaEnvironmentState()
+	{
+		containerSelectionHandler.DestroySelectedContainer();
+		SetSNRoomAreaEnvironmentState(false);
+		displayedContainerObject.SetActive(false);
+	}
+
+	private void ClearSNRoomEnvironmentState()
+	{
+		SetSNRoomAreaEnvironmentState(false);
+		containerPickerControlPanel.SetInteractable(false);
+		scientificNotationControlPanel.SetInteractable(false);
+		displayedContainerObject.SetActive(false);
+
+		scientificNotationTransitionDoor.GetComponent<Animator>().SetBool("door_closed", false); ;
+		scientificNotationTransitionBarrier.gameObject.SetActive(false);
+	}
+
+	#endregion
 }
