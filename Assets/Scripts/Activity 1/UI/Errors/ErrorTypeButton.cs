b57716603feee69cd962d49c3421727f641d2206
ndrw-ynz <andrewyanza4@gmail.com>
2024-10-16 00:29:23 +0800

    feat: add error type button UI script for selecting error types

---
+++ Assets/Scripts/Activity 1/UI/Errors/ErrorTypeButton.cs
@@ -0,0 +1,38 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class ErrorTypeButton : MonoBehaviour
+{
+	public static event Action UpdateClickedEvent;
+
+	public ErrorType errorType;
+	[SerializeField] private TextMeshProUGUI displayText;
+	public bool isClicked { get; private set; }
+
+	private void OnEnable()
+	{
+		UpdateClickedEvent += ResetState;
+	}
+
+	private void OnDisable()
+	{
+		UpdateClickedEvent -= ResetState;
+	}
+
+	public void ResetState()
+	{
+		isClicked = false;
+		displayText.color = new Color32(200, 75, 55, 255);
+	}
+
+	public void OnClick()
+	{
+		// Implemented on all button instances.
+		UpdateClickedEvent?.Invoke();
+
+		// Afterwards, this portion is only implemented on clicked instance.
+		isClicked = true;
+		displayText.color = new Color32(175, 255, 155, 255);
+	}
+}
