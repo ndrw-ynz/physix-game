44b68fa90bcc16506767450ab5070db2a31703a6
ndrw-ynz <andrewyanza4@gmail.com>
2024-10-22 17:54:05 +0800

    feat: add environment camera for cartesian components terminal area, and functionality for setting its environment state on open/close of associated terminal

--- Assets/Scripts/Activity 2/ActivityTwoEnvironmentManager.cs
+++ Assets/Scripts/Activity 2/ActivityTwoEnvironmentManager.cs
@@ -7,14 +7,19 @@ public class ActivityTwoEnvironmentManager : ActivityEnvironmentManager
 
 	[Header("Views")]
 	[SerializeField] private QuantitiesView quantitiesView;
+	[SerializeField] private CartesianComponentsView cartesianComponentsView;
 
 	[Header("Environment Cameras")]
 	[SerializeField] private Camera quantitiesTerminalAreaCamera;
+	[SerializeField] private Camera cartesianComponentsTerminalAreaCamera;
 
 	private void Start()
 	{
 		quantitiesView.OpenViewEvent += () => SetQuantitiesTerminalEnvironmentState(true);
 		quantitiesView.QuitViewEvent += () => SetQuantitiesTerminalEnvironmentState(false);
+
+		cartesianComponentsView.OpenViewEvent += () => SetCartesianComponentsTerminalEnvironmentState(true);
+		cartesianComponentsView.QuitViewEvent += () => SetCartesianComponentsTerminalEnvironmentState(false);
 	}
 
 	#region Quantities Terminal Area
@@ -25,5 +30,14 @@ public class ActivityTwoEnvironmentManager : ActivityEnvironmentManager
 		quantitiesTerminalAreaCamera.gameObject.SetActive(isActive);
 	}
 
+	#endregion
+
+	#region Cartesian Components Terminal Area
+	private void SetCartesianComponentsTerminalEnvironmentState(bool isActive)
+	{
+		SetPlayerActivityState(!isActive);
+		// activityTwoManager.SetMissionObjectiveDisplay(!isActive);
+		cartesianComponentsTerminalAreaCamera.gameObject.SetActive(isActive);
+	}
 	#endregion
 }
