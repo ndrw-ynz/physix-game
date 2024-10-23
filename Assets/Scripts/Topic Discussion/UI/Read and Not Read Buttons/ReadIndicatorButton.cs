a993ed804dc86c0159f8b3b3b1028a502cf03a80
Powell <powell.payofilin2400@gmail.com>
2024-10-23 17:35:36 +0800

    refactor: add and modify comments for read indicator display script

--- Assets/Scripts/Topic Discussion/UI/Read and Not Read Buttons/ReadIndicatorButton.cs
+++ Assets/Scripts/Topic Discussion/UI/Read and Not Read Buttons/ReadIndicatorButton.cs
@@ -8,16 +8,19 @@ public class ReadIndicatorButton : MonoBehaviour
 
     [Header("Flag To Use For Changing States")]
     [SerializeField] private ReadState state;
+
     [Header("Read Indicator Button")]
     [SerializeField] private Button _readIndicatorButton;
 
     private void OnEnable()
     {
+        // Add click listener for a read indicator button
         _readIndicatorButton.onClick.AddListener(() => ReadIndicatorClickEvent?.Invoke(state));
     }
 
     private void OnDisable()
     {
+        // Remove listeners of the button when the game object has been disabled
         _readIndicatorButton.onClick.RemoveAllListeners();
     }
 }
--- Assets/Scripts/Topic Discussion/UI/Read and Not Read Buttons/ReadIndicatorsDisplay.cs
+++ Assets/Scripts/Topic Discussion/UI/Read and Not Read Buttons/ReadIndicatorsDisplay.cs
@@ -11,13 +11,13 @@ public class ReadIndicatorsDisplay : MonoBehaviour
     {
         if (!isPageMarkedRead)
         {
-            // Activate the mark as read button
+            // Activate the mark as read button if page is not yet marked as read
             markAsReadButton.gameObject.SetActive(true);
             markAsNotReadButton.gameObject.SetActive(false);
         }
         else
         {
-            // Activate the mark as not yet read button
+            // Activate the mark as not yet read button if page has been marked as read
             markAsReadButton.gameObject.SetActive(false);
             markAsNotReadButton.gameObject.SetActive(true);
         }
