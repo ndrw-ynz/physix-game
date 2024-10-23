6019ae163e1e5d7e2eeed60bece79cab512a475b
ndrw-ynz <andrewyanza4@gmail.com>
2024-10-23 20:36:08 +0800

    fix: fix improper variable change on updating metrics variables for incorrect vector addition submission

--- Assets/Scripts/Activity 2/ActivityTwoManager.cs
+++ Assets/Scripts/Activity 2/ActivityTwoManager.cs
@@ -276,7 +276,7 @@ public class ActivityTwoManager : ActivityManager
 			numCorrectVectorAdditionSubmission++;
 		}  else
 		{
-			numIncorrectCartesianComponentsSubmission++;
+			numIncorrectVectorAdditionSubmission++;
 		}
 		DisplayVectorAdditionSubmissionResults(results);
 	}
