b873b15701fdae63441266494f95b9c6fc6df6d2
Powell <powell.payofilin2400@gmail.com>
2024-10-23 17:15:45 +0800

    refactor: add comments for previous and next sector and page button scripts

--- Assets/Scripts/Topic Discussion/UI/Previous and Next Buttons/SectorPrevNextButton.cs
+++ Assets/Scripts/Topic Discussion/UI/Previous and Next Buttons/SectorPrevNextButton.cs
@@ -9,18 +9,22 @@ public class SectorPrevNextButton : MonoBehaviour
 
     [Header("Subtopic Name Text Holder")]
     public TextMeshProUGUI sectorButtonText;
+
     [Header("Direction of Sector")]
     [SerializeField] private Direction direction;
+
     [Header("Sector Button")]
     [SerializeField] private Button _prevNextSectorButton;
 
     private void OnEnable()
     {
+        // Add click listener for a previous and next sector button
         _prevNextSectorButton.onClick.AddListener(() => SectorPrevNextClickEvent.Invoke(direction));
     }
 
     private void OnDisable()
     {
+        // Remove listeners of the button when the game object has been disabled
         _prevNextSectorButton.onClick.RemoveAllListeners();
     }
 }
