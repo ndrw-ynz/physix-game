2649285d1256750c491c6f6cada4c12d68e0f0d0
ndrw-ynz <andrewyanza4@gmail.com>
2024-10-28 21:41:46 +0800

    feat: add number of acceleration and total depth problems on SO level data for kinematics 1D

--- Assets/Scripts/Activity 3/Level Data SO/Kinematics1DSubActivitySO.cs
+++ Assets/Scripts/Activity 3/Level Data SO/Kinematics1DSubActivitySO.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Level Data/Kinematics1DSubActivitySO")]
+[CreateAssetMenu(menuName = "Level Data/Kinematics 1D")]
 public class Kinematics1DSubActivitySO : ScriptableObject
 {
-    [Header("Velocity Value Range")]
+	[Header("Number of Tests")]
+	public int numberOfAccelerationProblems;
+	public int numberOfTotalDepthProblems;
+	[Header("Velocity Value Range")]
     public int minimumVelocityValue;
     public int maximumVelocityValue;
     [Header("Time Value Range")]
