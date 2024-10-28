5e1e8e44e9f4d4397a9daaab6af102584d61ec52
ndrw-ynz <andrewyanza4@gmail.com>
2024-10-28 12:53:30 +0800

    feat: add functionality for clearing graphs terminal environment state on accomplishment of sub activity, from GraphsAreaClearEvent

--- Assets/Scripts/Activity 3/ActivityThreeEnvironmentManager.cs
+++ Assets/Scripts/Activity 3/ActivityThreeEnvironmentManager.cs
@@ -2,16 +2,30 @@ using UnityEngine;
 
 public class ActivityThreeEnvironmentManager : ActivityEnvironmentManager
 {
+	[Header("Activity Manager")]
+	[SerializeField] private ActivityThreeManager activityThreeManager;
+
 	[Header("Views")]
 	[SerializeField] private GraphsView graphsView;
 
 	[Header("Environment Cameras")]
 	[SerializeField] private Camera graphsEnvironmentCamera;
 
+	[Header("Interactable Terminal Game Objects")]
+	[SerializeField] private InteractableViewOpenerObject graphsTerminal;
+
+	[Header("Doors")]
+	[SerializeField] private GameObject graphsTransitionDoor;
+
 	private void Start()
 	{
 		graphsView.OpenViewEvent += () => SetGraphsTerminalEnvironmentState(true);
 		graphsView.QuitViewEvent += () => SetGraphsTerminalEnvironmentState(false);
+
+		activityThreeManager.GraphsAreaClearEvent += ClearGraphsTerminalEnvironmentState;
+
+		// Close door on start
+		graphsTransitionDoor.GetComponent<Animator>().SetBool("door_open", false);
 	}
 
 	#region Graphs Terminal Area
@@ -20,6 +34,13 @@ public class ActivityThreeEnvironmentManager : ActivityEnvironmentManager
 		SetPlayerActivityState(!isActive);
 		// activityThreeManager.SetMissionObjectiveDisplay(!isActive);
 		graphsEnvironmentCamera.gameObject.SetActive(isActive);
+	}
+
+	private void ClearGraphsTerminalEnvironmentState()
+	{
+		SetGraphsTerminalEnvironmentState(false);
+		graphsTerminal.SetInteractable(false);
+		graphsTransitionDoor.GetComponent<Animator>().SetBool("door_open", true);
 	}
 	#endregion
 }
