7e300afdfd82a0e70356705eca77df0722a402b6
Powell <powell.payofilin2400@gmail.com>
2024-10-30 17:48:38 +0800

    feat: add hover effect for lesson components buttons

--- Assets/Scripts/Main Menu/Buttons/Lesson Components Select Screen/ActivityButton.cs
+++ Assets/Scripts/Main Menu/Buttons/Lesson Components Select Screen/ActivityButton.cs
@@ -1,17 +1,23 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ActivityButton : MonoBehaviour
+public class ActivityButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public static event Action<int> ActivityClick;
 
     [Header("Activity Button Properties")]
-    [SerializeField] private GameObject activityPanel;
+    [SerializeField] private Image activityPanel;
+    [SerializeField] private TextMeshProUGUI panelText;
     [SerializeField] private Button activityButton;
 
     [Header("Activity Button Number")]
     [SerializeField] private int activityNumber;
+
+    // Boolean to check if button is being hovered or not
+    private bool isHovered;
 
     private void OnEnable()
     {
@@ -23,5 +29,19 @@ public class ActivityButton : MonoBehaviour
     {
         // Unsubscribe Button Click Listeners
         activityButton.onClick.RemoveAllListeners();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        activityPanel.color = Color.black;
+        panelText.color = Color.white;
+        isHovered = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        activityPanel.color = Color.white;
+        panelText.color = Color.black;
+        isHovered = false;
     }
 }
--- Assets/Scripts/Main Menu/Buttons/Lesson Components Select Screen/TopicDiscussionButton.cs
+++ Assets/Scripts/Main Menu/Buttons/Lesson Components Select Screen/TopicDiscussionButton.cs
@@ -1,17 +1,23 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class TopicDiscussionButton : MonoBehaviour
+public class TopicDiscussionButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public static event Action<int> TopicDiscussionClick;
 
     [Header("Topic Discussion Button Properties")]
-    [SerializeField] private GameObject topicDiscussionPanel;
+    [SerializeField] private Image topicDiscussionPanel;
+    [SerializeField] private TextMeshProUGUI panelText;
     [SerializeField] private Button topicDiscussionButton;
 
     [Header("Topic Discussion Button Number")]
     [SerializeField] private int topicDiscussionNumber;
+
+    // Boolean to check if button is being hovered or not
+    private bool isHovered;
 
     private void OnEnable()
     {
@@ -23,5 +29,32 @@ public class TopicDiscussionButton : MonoBehaviour
     {
         // Unsubscribe Button Click Listeners
         topicDiscussionButton.onClick.RemoveAllListeners();
+
+        // Ensure hover color is removed and set back to normal on disable
+        if (isHovered)
+        {
+            topicDiscussionPanel.color = Color.white;
+            panelText.color = Color.black;
+            isHovered = false;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // Change panel color to black and text to white on hover for topic discussion button
+        topicDiscussionPanel.color = Color.black;
+        panelText.color = Color.white;
+        isHovered = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // Change panel color to white and text to black on hover for current hovered button
+        if (isHovered)
+        {
+            topicDiscussionPanel.color = Color.white;
+            panelText.color = Color.black;
+            isHovered = false;
+        }
     }
 }
