8e25e65db5dd1f91ee473f967d178ad18d70daf6
ndrw-ynz <andrewyanza4@gmail.com>
2024-10-31 22:16:07 +0800

    refactor: delegate validation of projectile motion answers to ValidateProjectileMotionSubmission function, with correct and appropriate formulas

--- Assets/Scripts/Activity 4/ActivityFourUtilities.cs
+++ Assets/Scripts/Activity 4/ActivityFourUtilities.cs
@@ -1,29 +1,60 @@
+using System;
 using UnityEngine;
+
+public class ProjectileMotionSubmissionResults
+{
+	public bool isMaximumHeightCorrect;
+	public bool isHorizontalRangeCorrect;
+	public bool isTimeOfFlightCorrect;
+
+	public bool isAllCorrect()
+	{
+		return isMaximumHeightCorrect && isHorizontalRangeCorrect && isTimeOfFlightCorrect;
+	}
+}
 
 public static class ActivityFourUtilities
 {
-	public static bool ValidateMaximumHeightSubmission(float submittedMaximumHeight, float projectileInitialVelocity, float projectileAngle)
+	public static ProjectileMotionSubmissionResults ValidateProjectileMotionSubmission(ProjectileMotionAnswerSubmission answer, ProjectileMotionCalculationData givenData)
 	{
-		ExpressionEvaluator.Evaluate($"-({projectileInitialVelocity})^2*sin({projectileAngle}*(pi/180))/(2*-9.81)", out float computationResult);
-		Debug.Log($"computed result: {computationResult}");
-		Debug.Log($"submitted : {submittedMaximumHeight}");
-		return Mathf.Abs(submittedMaximumHeight - computationResult) <= 0.0001;
-	}
+		ProjectileMotionSubmissionResults results = new ProjectileMotionSubmissionResults();
 
-	public static bool ValidateHorizontalRangeSubmission(float submittedHorizontalRange, float projectileInitialVelocity)
-	{
-		ExpressionEvaluator.Evaluate($"-({projectileInitialVelocity})^2*sin(pi/2)/(2*-9.81)", out float computationResult);
-		Debug.Log($"computed result: {computationResult}");
-		Debug.Log($"submitted : {submittedHorizontalRange}");
-		return Mathf.Abs(submittedHorizontalRange - computationResult) <= 0.0001;
-	}
+		// Validate maximum height submission
+		if (answer.maximumHeight != null)
+		{
+			ExpressionEvaluator.Evaluate($"(- ({givenData.initialVelocity}^2) * (sin({givenData.angleMeasure}*(pi/180)))^2) / (2 * -9.81)) + {givenData.initialHeight}", out float computedMaximumHeight);
+			computedMaximumHeight = (float) Math.Round(computedMaximumHeight, 2);
+			results.isMaximumHeightCorrect = Mathf.Abs(computedMaximumHeight - (float) answer.maximumHeight) <= 0.0001;
+		} else
+		{
+			results.isMaximumHeightCorrect = false;
+		}
 
-	public static bool ValidateTimeOfFlightSubmission(float timeOfFlight, float projectileInitialVelocity, float projectileAngle)
-	{
-		ExpressionEvaluator.Evaluate($"-({projectileInitialVelocity})*sin({projectileAngle}*(pi/180))/-9.81", out float computationResult);
-		Debug.Log($"computed result: {computationResult}");
-		Debug.Log($"submitted : {timeOfFlight}");
-		return Mathf.Abs(timeOfFlight - computationResult) <= 0.0001;
+		// Validate horizontal range submission
+		if (answer.horizontalRange != null)
+		{
+			ExpressionEvaluator.Evaluate($"(- ({givenData.initialVelocity})^2 * sin(2*{givenData.angleMeasure}*(pi/180))) / (-9.81)", out float computedHorizontalRange);
+			computedHorizontalRange = (float)Math.Round(computedHorizontalRange, 2);
+			results.isHorizontalRangeCorrect = Mathf.Abs(computedHorizontalRange - (float)answer.horizontalRange) <= 0.0001;
+		}
+		else
+		{
+			results.isHorizontalRangeCorrect = false;
+		}
+
+		// Validate time of flight answer submission
+		if (answer.timeOfFlight != null)
+		{
+			ExpressionEvaluator.Evaluate($"-(2 * {givenData.initialVelocity} * sin({givenData.angleMeasure}*(pi/180))) / (-9.81)", out float computedTimeOfFlight);
+			computedTimeOfFlight = (float)Math.Round(computedTimeOfFlight, 2);
+			results.isTimeOfFlightCorrect = Mathf.Abs(computedTimeOfFlight - (float)answer.timeOfFlight) <= 0.0001;
+		}
+		else
+		{
+			results.isTimeOfFlightCorrect = false;
+		}
+
+		return results;
 	}
 
 	public static bool ValidateCentripetalAccelerationSubmission(float centripetalAcceleration, float satelliteRadius, float satelliteTimePeriod)
