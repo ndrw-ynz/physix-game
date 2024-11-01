48a6bebd7d8c619299b637f619fe0704f2ca4ef9
ndrw-ynz <andrewyanza4@gmail.com>
2024-11-01 12:25:35 +0800

    fix: decrease decimal value difference check in validating centripetal acceleration calc submission

--- Assets/Scripts/Activity 4/ActivityFourUtilities.cs
+++ Assets/Scripts/Activity 4/ActivityFourUtilities.cs
@@ -63,6 +63,6 @@ public static class ActivityFourUtilities
 
 		ExpressionEvaluator.Evaluate($"(4*(pi^2)*{givenData.radius*1000})/({givenData.period}^2)", out float computedCentripetalAcceleration);
 		computedCentripetalAcceleration = (float)Math.Round(computedCentripetalAcceleration, 4);
-		return Mathf.Abs((float)centripetalAccelerationAnswer - computedCentripetalAcceleration) <= 0.0001;
+		return Mathf.Abs((float)centripetalAccelerationAnswer - computedCentripetalAcceleration) <= 0.01;
 	}
 }
