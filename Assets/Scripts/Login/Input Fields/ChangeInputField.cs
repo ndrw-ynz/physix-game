f0cb865595c5b2b2437b82bbdab2adac7c6176ac
Powell <powell.payofilin2400@gmail.com>
2024-11-05 18:14:48 +0800

    feat: add input field switch using tab or tab+lshift

---
+++ Assets/Scripts/Login/Input Fields/ChangeInputField.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ChangeInputField : MonoBehaviour
+{
+    EventSystem eventSystem;
+
+    [Header("First Input Field")]
+    [SerializeField] Selectable firstInput;
+    void Start()
+    {
+        eventSystem = EventSystem.current;
+        firstInput.Select();
+    }
+
+    void Update()
+    {
+        // Switch between input fields using tab or tab + lshift
+        if (Input.GetKeyDown(KeyCode.Tab) && Input.GetKey(KeyCode.LeftShift)){
+            Selectable previous = eventSystem.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnUp();
+            if (previous != null)
+            {
+                previous.Select();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selectable next = eventSystem.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+            if(next != null)
+            {
+                next.Select();
+            }
+        }
+    }
+}
