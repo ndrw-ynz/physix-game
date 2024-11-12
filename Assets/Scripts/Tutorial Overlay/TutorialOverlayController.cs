97d0e25463f9b81964d13a91e6a1433f55641853
Powell <powell.payofilin2400@gmail.com>
2024-11-12 14:30:06 +0800

    refactor: add comments for tutorial overlay controller script

--- Assets/Scripts/Tutorial Overlay/TutorialOverlayController.cs
+++ Assets/Scripts/Tutorial Overlay/TutorialOverlayController.cs
@@ -10,22 +10,25 @@ public class TutorialOverlayController : MonoBehaviour
         public GameObject terminalStatusScreenUI;
     }
 
+    [Header("Game Objects To Be Checked")]
     [SerializeField] private List<TerminalStatusScreenGroup> terminalStatusScreenGroup;
     [SerializeField] private List<GameObject> noStatusScreenTerminals;
     [SerializeField] private TutorialOverlayDisplay tutorialDisplay;
-
     [SerializeField] private GameObject pauseMenuUI;
     [SerializeField] private GameObject performanceResultsUI;
 
+    [Header("Input Reader")]
     [SerializeField] protected  InputReader inputReader;
 
     private void OnEnable()
     {
+        // Add subscriber for resuming gameplay upon closing tutorial oerlay
         TutorialOverlayDisplay.TutorialOverlayClose += ResumeGameplay;
     }
 
     private void OnDisable()
     {
+        // Remove subscriber
         TutorialOverlayDisplay.TutorialOverlayClose -= ResumeGameplay;
     }
 
@@ -33,61 +36,16 @@ public class TutorialOverlayController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            // If tutorial overlay is active, close tutorial overlay
-            if (tutorialDisplay.gameObject.activeSelf)
-            {
-                tutorialDisplay.gameObject.SetActive(false);
-                ResumeGameplay();
-                return;
-            }
+            // Checker for closing tutorial overlay upon pressing 'T' again
+            CheckForActiveTutorialDisplay();
 
-            // If pause menu UI is active, don't open tutorial overlay
-            if (pauseMenuUI.activeSelf)
-            {
-                return;
-            }
+            // Checkers for avoiding tutorial overlay opening if specific game objects are active
+            CheckForActivePauseMenu();
+            CheckForActivePerformanceResult();
 
-            // If performance results UI is active, dont open tutorial overlay
-            if (performanceResultsUI.activeSelf)
-            {
-                return;
-            }
-
-            // If no results screen terminals list count is more than 0, loop through list
-            if (noStatusScreenTerminals.Count > 0)
-            {
-                // Check if there's an active no results screen terminal, if yes, open tutorial overlay
-                // I might have only done this for activity one since it has a container picker terminal
-                for (int i = 0; i < noStatusScreenTerminals.Count; i++)
-                {
-                    if (noStatusScreenTerminals[i].gameObject.activeSelf)
-                    {
-                        tutorialDisplay._currentSectorIndex = 5;
-                        tutorialDisplay.gameObject.SetActive(true);
-                        inputReader.SetUI();
-                    }
-                }
-            }
-
-            // Loop through the terminaal results screen group
-            for (int i = 0; i < terminalStatusScreenGroup.Count; i++)
-            {
-                // If a sub activity terminal is active, and its results screen is not active,
-                // open the tutorial overlay to the solvings section's first page
-                if (terminalStatusScreenGroup[i].terminalStatusScreenUI.activeSelf)
-                {
-                    return;
-                }
-
-                if (terminalStatusScreenGroup[i].terminalUI.activeSelf)
-                {
-                    // Load the solving's tutorial first sector page
-                    tutorialDisplay._currentSectorIndex = 5;
-                    tutorialDisplay.gameObject.SetActive(true);
-                    inputReader.SetUI();
-                    return;
-                }
-            }
+            // Checkers for opening directly to the calculations section of the tutorial overlay
+            CheckForActiveNoStatusScreenTerminals();
+            CheckForActiveOnlySubActivityTerminals();
 
             // Open the tutorial display normally at the first page of the gameplay section.
             // Will not open directly to the solvings area since no terminals are opened
@@ -97,6 +55,84 @@ public class TutorialOverlayController : MonoBehaviour
     }
 
     private void ResumeGameplay()
+    {
+        CheckForActiveSubActivityRelatedScreen();
+
+        // Go back to gameplay mode if there is no active terminals
+        inputReader.SetGameplay();
+    }
+
+    #region On Tutorial Overlay Open Game Object Checkers
+    private void CheckForActiveTutorialDisplay()
+    {
+        // If tutorial overlay is active, close tutorial overlay
+        if (tutorialDisplay.gameObject.activeSelf)
+        {
+            tutorialDisplay.gameObject.SetActive(false);
+            ResumeGameplay();
+            return;
+        }
+    }
+    private void CheckForActivePauseMenu()
+    {
+        // If pause menu UI is active, don't open tutorial overlay
+        if (pauseMenuUI.activeSelf)
+        {
+            return;
+        }
+    }
+    private void CheckForActivePerformanceResult()
+    {
+        // If performance results UI is active, dont open tutorial overlay
+        if (performanceResultsUI.activeSelf)
+        {
+            return;
+        }
+    }
+    private void CheckForActiveNoStatusScreenTerminals()
+    {
+        // If no results screen terminals list count is more than 0, loop through list
+        if (noStatusScreenTerminals.Count > 0)
+        {
+            // Check if there's an active no results screen terminal, if yes, open tutorial overlay
+            // I might have only done this for activity one since it has a container picker terminal
+            for (int i = 0; i < noStatusScreenTerminals.Count; i++)
+            {
+                if (noStatusScreenTerminals[i].gameObject.activeSelf)
+                {
+                    tutorialDisplay._currentSectorIndex = 5;
+                    tutorialDisplay.gameObject.SetActive(true);
+                    inputReader.SetUI();
+                }
+            }
+        }
+    }
+    private void CheckForActiveOnlySubActivityTerminals()
+    {
+        // Loop through the terminal results screen group
+        for (int i = 0; i < terminalStatusScreenGroup.Count; i++)
+        {
+            // If a sub activity terminal is active, and its results screen is not active,
+            // open the tutorial overlay to the solvings section's first page
+            if (terminalStatusScreenGroup[i].terminalStatusScreenUI.activeSelf)
+            {
+                return;
+            }
+
+            if (terminalStatusScreenGroup[i].terminalUI.activeSelf)
+            {
+                // Load the solving's tutorial first sector page
+                tutorialDisplay._currentSectorIndex = 5;
+                tutorialDisplay.gameObject.SetActive(true);
+                inputReader.SetUI();
+                return;
+            }
+        }
+    }
+    #endregion
+
+    #region On Tutorial Overlay Close Game Object Checkers
+    private void CheckForActiveSubActivityRelatedScreen()
     {
         // If no results screen terminals list count is more than 0, loop through list
         if (noStatusScreenTerminals.Count > 0)
@@ -121,8 +157,6 @@ public class TutorialOverlayController : MonoBehaviour
                 return;
             }
         }
-
-        // Go back to gameplay mode if there is no active terminals
-        inputReader.SetGameplay();
     }
+    #endregion
 }
