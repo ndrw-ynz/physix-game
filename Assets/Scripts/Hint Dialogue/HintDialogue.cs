dad74f5d861f94f0568fb29ceb176a9ad0d49dc1
Powell <powell.payofilin2400@gmail.com>
2024-11-29 21:57:51 +0800

    refactor: add more comments for hint dialogue script

--- Assets/Scripts/Hint Dialogue/HintDialogue.cs
+++ Assets/Scripts/Hint Dialogue/HintDialogue.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,35 +9,46 @@ public class HintDialogue : MonoBehaviour
 {
     public static event Action HintDialogueFinished;
 
+    [Header("Dialogue Properties")]
     [SerializeField] private TextMeshProUGUI dialogueText;
     [SerializeField] private Button backgroundButton;
-    [SerializeField] private GameObject downArrowButton;
+    [SerializeField] private GameObject downArrow;
     public List<string> sentences = new List<string>();
+
+    // Current index of the sentences list
     private int index;
 
     void OnEnable()
     {
         // Reset index to 0
         index = 0;
-        downArrowButton.SetActive(false);
+
+        // Deactivate down arrow
+        downArrow.SetActive(false);
+
+        // Disable the background button
         backgroundButton.enabled = false;
 
+        // Start typing the sentence
         StartCoroutine(Type());
     }
 
     void Update()
     {
+        // If the sentence is complete, activate down arrow and enable background button to be clicked
         if (dialogueText.text == sentences[index])
         {
-            downArrowButton.SetActive(true);
+            downArrow.SetActive(true);
             backgroundButton.enabled = true;
         }
     }
 
     public IEnumerator Type()
     {
+        // Clear dialogue text
         dialogueText.text = "";
 
+        // Create a typing effect for the current sentence
         foreach (char letter in sentences[index].ToCharArray())
         {
             dialogueText.text += letter;
@@ -48,9 +58,13 @@ public class HintDialogue : MonoBehaviour
 
     public void NextSentence()
     {
-        downArrowButton.SetActive(false);
+        // Deactivate down arrow
+        downArrow.SetActive(false);
+
+        // Disable button background
         backgroundButton.enabled = false;
 
+        // Check if there's still more sentences to show in dialogue or not
         if (index < sentences.Count - 1)
         {
             index++;
